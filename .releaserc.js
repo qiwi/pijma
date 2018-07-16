b4090569c9a80d6768726723dd540e4b5d599b60
Anton Golub <mailbox@antongolub.ru>
2018-07-16 13:31:01 +0300

    build: migrate to `semantic-release-monorepo-hooks`

---
+++ .releaserc.js
@@ -0,0 +1,30 @@
+const hooks = require('semantic-release-monorepo-hooks')
+const output = hooks()
+
+const publish = output.isLastRun
+  ? [
+    '@semantic-release/github',
+    '@semantic-release/npm'
+  ]
+  : [
+    '@semantic-release/npm'
+  ]
+
+module.exports = {
+  branch: 'master',
+  tagFormat: 'v${version}',
+  prepare: [
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    '@semantic-release/git'
+  ],
+  publish: publish,
+  verifyConditions: [
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    '@semantic-release/git'
+  ]
+  /* verifyRelease: ['@semantic-release/npm', '@semantic-release/github']
+    .map(require)
+    .map(x => x.verifyConditions), */
+};
