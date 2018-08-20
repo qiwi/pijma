3c0dba5dc5ded11744ce774a4a69e82abe79fcd4
Anton Golub <mailbox@antongolub.ru>
2018-08-20 12:30:00 +0300

    feat(media): add 48*24 icons Visa, Mastercard, Mir

---
+++ packages/media/src/icon/MastercardIcon.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const MastercardIcon: React.SFC = () => (
+  <svg viewBox="0 0 48 24" focusable="false">
+    <path
+      fill="#FC9F3A"
+      d="M14.82 7.62c0 4.076-3.298 7.38-7.366 7.38S.088 11.696.088 7.62C.088 3.544 3.386.24 7.454.24s7.366 3.304 7.366 7.38z"
+      transform="translate(21 4.76)"
+    />
+    <path
+      fill="#F14840"
+      d="M14.732 7.62c0 4.076-3.297 7.38-7.365 7.38S0 11.696 0 7.62C0 3.544 3.3.24 7.367.24c4.067 0 7.365 3.304 7.365 7.38z"
+      transform="translate(12 4.76)"
+    />
+    <path
+      fill="#F46B3E"
+      d="M23.91 6.57a7.37 7.37 0 0 0-2.822 5.81 7.372 7.372 0 0 0 2.822 5.81 7.372 7.372 0 0 0 2.822-5.81 7.37 7.37 0 0 0-2.822-5.81"
+    />
+  </svg>
+)
+
+export default MastercardIcon
--- packages/mobile/styleguide/examples/IconsExample.tsx
+++ packages/mobile/styleguide/examples/IconsExample.tsx
@@ -10,6 +10,13 @@ const IconWrapper = styled('span')({
   margin: 8,
 })
 
+const PaysysIconWrapper = styled('span')({
+  display: 'inline-block',
+  width: 48,
+  height: 24,
+  margin: 8,
+})
+
 const iconNames: IconName[] = [
   'calendar',
   'clock',
@@ -29,6 +36,12 @@ const iconNames: IconName[] = [
   'star',
 ]
 
+const paysysIcons: IconName[] = [
+  'mastercard',
+  'mir',
+  'visa',
+]
+
 export default class IconsExample extends Component {
 
   public render() {
@@ -38,6 +51,11 @@ export default class IconsExample extends Component {
           <IconWrapper key={name}>
             <Icon name={name}/>
           </IconWrapper>
+        ))}
+        {paysysIcons.map(name => (
+          <PaysysIconWrapper key={name}>
+            <Icon name={name}/>
+          </PaysysIconWrapper>
         ))}
       </Fragment>
     )
