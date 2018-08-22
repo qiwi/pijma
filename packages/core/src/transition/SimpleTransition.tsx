080dee15484f20032d1891a44c57d99b4d96f6ec
Dima Livshits <dimonka83@gmail.com>
2018-08-22 12:03:34 +0300

    perf: up deps

--- packages/core/src/transition/SimpleTransition.tsx
+++ packages/core/src/transition/SimpleTransition.tsx
@@ -5,32 +5,44 @@ import Transition, {ENTERED, ENTERING, EXITED, EXITING} from 'react-transition-g
 import SimpleTransitionProps from './SimpleTransitionProps'
 
 const statusClassName = (status: string, props: SimpleTransitionProps): string | undefined => {
-  const enter = typeof props.timeout === 'number' ? props.timeout : props.timeout.enter || 0
-  const exit = typeof props.timeout === 'number' ? props.timeout : props.timeout.exit || 0
+  const {
+    timeout,
+    enteringClassName,
+    enteredClassName,
+    enterClassName,
+    exitingClassName,
+    exitedClassName,
+    exitClassName,
+  } = props
+  const enter = typeof timeout === 'number' ? timeout : timeout.enter || 0
+  const exit = typeof timeout === 'number' ? timeout : timeout.exit || 0
   switch (status) {
     case ENTERING:
-      return props.enteringClassName ? props.enteringClassName(enter) : props.enterClassName ? props.enterClassName(enter) : undefined
+      return enteringClassName ? enteringClassName(enter) : enterClassName ? enterClassName(enter) : undefined
     case ENTERED:
-      return props.enteredClassName ? props.enteredClassName(enter) : props.enterClassName ? props.enterClassName(enter) : undefined
+      return enteredClassName ? enteredClassName(enter) : enterClassName ? enterClassName(enter) : undefined
     case EXITING:
-      return props.exitingClassName ? props.exitingClassName(exit) : props.exitClassName ? props.exitClassName(enter) : undefined
+      return exitingClassName ? exitingClassName(exit) : exitClassName ? exitClassName(enter) : undefined
     case EXITED:
-      return props.exitedClassName ? props.exitedClassName(exit) : props.exitClassName ? props.exitClassName(enter) : undefined
+      return exitedClassName ? exitedClassName(exit) : exitClassName ? exitClassName(enter) : undefined
   }
   return undefined
 }
 
 
-const SimpleTransition: React.SFC<SimpleTransitionProps> = ({children, ...props}) => (
-  <Transition {...props}>
-    {React.isValidElement(children) ? (
-      (status: string) => React.cloneElement(children, {
-        className: cx(children.props.className, statusClassName(status, props)),
-      })
-    ) : (
-      children
-    )}
-  </Transition>
-)
+const SimpleTransition: React.SFC<SimpleTransitionProps> = ({children, ...props}) => {
+  const child = children as React.ReactElement<{className?: string}>
+  return (
+    <Transition {...props}>
+      {React.isValidElement(children) && React.Children.only(child) ? (
+        (status) => React.cloneElement(child, {
+          className: cx(child.props.className, statusClassName(status, props)),
+        })
+      ) : (
+        children
+      )}
+    </Transition>
+  )
+}
 
 export default SimpleTransition
