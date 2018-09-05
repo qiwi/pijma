0800061d0cdc2b479472e3b7d8f0e76b52015d4a
Anton Golub <mailbox@antongolub.ru>
2018-09-05 16:35:42 +0300

    feat(media): extend icon pack

---
+++ packages/media/src/icon/AngleSmallRightIcon.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AngleSmallRightIcon: React.SFC = () => (
+  <svg viewBox="0 0 24 24" focusable="false">
+    <path
+      d="M8.293 7.707a1 1 0 0 1 1.414-1.414l5 5a1 1 0 0 1 0 1.414l-5 5a1 1 0 0 1-1.414-1.414L12.586 12 8.293 7.707z"
+    />
+  </svg>
+)
+
+export default AngleSmallRightIcon
---
+++ packages/media/src/icon/AngleUpIcon.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AngleUpIcon: React.SFC = () => (
+  <svg viewBox="0 0 24 24" focusable="false">
+    <path
+      d="M12.456 9.415l-7.77 7.313a1 1 0 1 1-1.371-1.456l8.5-8a1 1 0 0 1 1.415.044l7.5 8a1 1 0 1 1-1.46 1.368l-6.814-7.27z"
+    />
+  </svg>
+)
+
+export default AngleUpIcon
