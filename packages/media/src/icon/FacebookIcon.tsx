f032ce31dd9b2d6a6867ac3616072922265ea051
a.nenashev <qq89276505884>
2018-09-06 14:46:44 +0300

    fix: focusable false

--- packages/media/src/icon/FacebookIcon.tsx
+++ packages/media/src/icon/FacebookIcon.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const FacebookIcon: React.SFC = () => (
-  <svg viewBox="0 0 24 24">
+  <svg viewBox="0 0 24 24" focusable="false">
     <path d="M9.3 9H7v3h2.3v9H13v-9h2.7l.3-3h-3V7.7c0-.7.1-1 .8-1H16V3h-2.9c-2.7 0-3.8 1.2-3.8 3.5V9z"/>
   </svg>
 )
--- packages/media/src/icon/YoutubeIcon.tsx
+++ packages/media/src/icon/YoutubeIcon.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const YoutubeIcon: React.SFC = () => (
-  <svg viewBox="0 0 24 24">
+  <svg viewBox="0 0 24 24" focusable="false">
     <path d="M18.3 4.2c-3-.3-9.6-.3-12.6 0C2.4 4.4 2 6.3 2 11.5c0 5.2.4 7.1 3.7 7.3 3 .3 9.6.3 12.6 0 3.3-.2 3.7-2.1 3.7-7.3 0-5.2-.4-7.1-3.7-7.3zM9.5 14.8V8.2l6.7 3.3-6.7 3.3z"/>
   </svg>
 )
