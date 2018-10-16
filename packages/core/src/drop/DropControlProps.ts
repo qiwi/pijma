3f8ceed013fdcd5f97a08a9c140a4e9d39a6349e
Margarita Yanochkina <yanochkina.mv@gmail.com>
2018-10-16 18:09:43 +0300

    fix: onhide handler type

--- packages/core/src/drop/DropControlProps.ts
+++ packages/core/src/drop/DropControlProps.ts
@@ -1,11 +1,9 @@
-import React from 'react'
-
 import RenderChild from '../RenderChild'
 
 export default interface DropControlProps {
   onHide: () => void
   children: RenderChild<{
-    onHide: React.FocusEventHandler
+    onHide: () => void
   }>
 }
 
