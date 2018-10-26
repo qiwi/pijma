54f545a5183b82d3c6a31be71eac505222a1b3ad
Maxim Pismenskiy <m.pismenskiy@qiwi.com>
2018-10-26 11:41:06 +0300

    feat(media): add pagination icons

---
+++ packages/media/src/icon/AngleDoubleRightIcon.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AngleDoubleRightIcon: React.SFC = () => (
+    <svg viewBox="0 0 24 24" focusable="false">
+        <path
+            d="M18.6,12l-7.3,7.3c-0.4,0.4-0.4,1,0,1.4c0.4,0.4,1,0.4,1.4,0l8-8c0.4-0.4,0.4-1,0-1.4l-8-8
+	c-0.4-0.4-1-0.4-1.4,0c-0.4,0.4-0.4,1,0,1.4L18.6,12z M10.6,12l-7.3,7.3c-0.4,0.4-0.4,1,0,1.4c0.4,0.4,1,0.4,1.4,0l8-8
+	c0.4-0.4,0.4-1,0-1.4l-8-8c-0.4-0.4-1-0.4-1.4,0c-0.4,0.4-0.4,1,0,1.4L10.6,12z"
+        />
+    </svg>
+)
+
+export default AngleDoubleRightIcon
