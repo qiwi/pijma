1aa64fd339425797e8e2c33780d5cfe8598a3da9
Dima Livshits <dimonka83@gmail.com>
2018-11-30 13:33:02 +0300

    chore: basic input

---
+++ packages/core/src/input/BasicInput.tsx
@@ -0,0 +1,81 @@
+import React, {FunctionComponent, ChangeEventHandler, FocusEventHandler, KeyboardEventHandler} from 'react'
+
+import {MaskInput, Input, isMaskDigital, Mask, Pipe} from '@qiwi/pijma-core'
+
+export interface BasicInputProps {
+  value: string
+  tabIndex?: number
+  type?: 'text' | 'password' | 'tel' | 'number' | 'search' | 'email' | 'url'
+  name?: string
+  autoComplete?: boolean
+  autoFocus?: boolean
+  placeholder?: string
+  maxLength?: number
+  padded: boolean
+  disabled?: boolean
+  error: boolean
+  focused: boolean
+  mask?: Mask
+  pipe?: Pipe
+  onChange?: ChangeEventHandler
+  onFocus?: FocusEventHandler
+  onBlur?: FocusEventHandler
+  onKeyDown?: KeyboardEventHandler
+  onKeyUp?: KeyboardEventHandler
+}
+
+export const BasicInput: FunctionComponent<BasicInputProps> = (props) => {
+  const common = {
+    width: 1,
+    height: 7,
+    m: 0,
+    p: 0,
+    pr: props.padded ? 7 : undefined,
+    r: 0,
+    b: 'none',
+    bb: props.disabled ? '1px dotted #999' : props.error ? '2px solid #d0021b' : props.focused ? '2px solid #ff8c00' : '1px solid rgba(0, 0, 0, 0.2)',
+    valueSize: 5,
+    valueWeight: 300,
+    valueColor: props.disabled ? '#666' : '#000',
+    placeholderColor: '#666',
+    cursor: props.disabled ? 'not-allowed' : 'text',
+    bg: 'transparent',
+    transition: 'all 100ms cubic-bezier(0.4, 0.0, 0.2, 1)',
+    value: props.value,
+    name: props.name,
+    autoComplete: props.autoComplete ? 'on' : 'off',
+    autoFocus: props.autoFocus,
+    placeholder: props.placeholder,
+    disabled: !!props.disabled,
+    maxLength: props.maxLength,
+    onChange: props.onChange,
+    onFocus: props.onFocus,
+    onBlur: props.onBlur,
+    onKeyDown: props.onKeyDown,
+    onKeyUp: props.onKeyUp,
+  }
+  return (
+    props.mask ? (
+      <MaskInput
+        {...common}
+        type={props.type === undefined ? (isMaskDigital(props.mask) ? 'tel' : 'text') : (['text', 'password', 'tel'].includes(props.type) ? props.type : 'text')}
+        mask={props.mask}
+        pipe={props.pipe}
+        guide={false}
+        keepCharPositions={!!props.pipe}
+        placeholderChar={'\u2000'}
+      />
+    ) : (
+      <Input
+        {...common}
+        type={props.type === undefined ? 'text' : props.type}
+      />
+    )
+  )
+}
+
+BasicInput.defaultProps = {
+  tabIndex: 0,
+}
+
+export default BasicInput
--- packages/desktop/src/password-field/PasswordField.tsx
+++ packages/desktop/src/password-field/PasswordField.tsx
@@ -1,6 +1,6 @@
 import React, {FunctionComponent} from 'react'
 
-import {Box, Input, MaskInput, InputField, PasswordFieldControl} from '@qiwi/pijma-core'
+import {PasswordFieldControl, Box, InputField, BasicInput} from '@qiwi/pijma-core'
 import {EyeClosedIcon, EyeOpenedIcon} from '@qiwi/pijma-media'
 
 import PasswordFieldProps from './PasswordFieldProps'
@@ -18,67 +18,21 @@ const PasswordField: FunctionComponent<PasswordFieldProps> = (props) => (
         title={props.title}
         active={renderProps.focused || !!props.value || !!props.placeholder}
         padded={!!props.hint || !!props.viewed}
-        input={props.mask ? (
-          <MaskInput
-            width={1}
-            height={7}
-            m={0}
-            p={0}
-            pr={props.hint ? 7 : undefined}
-            r={0}
-            b="none"
-            bb={props.disabled ? '1px dotted #999' : props.error ? '2px solid #d0021b' : renderProps.focused ? '2px solid #ff8c00' : '1px solid rgba(0, 0, 0, 0.2)'}
-            size={5}
-            weight={300}
-            color={props.disabled ? '#666' : '#000'}
-            placeholderColor="#666"
-            cursor={props.disabled ? 'not-allowed' : 'text'}
-            bg="transparent"
-            transition="all 100ms cubic-bezier(0.4, 0.0, 0.2, 1)"
+        input={(
+          <BasicInput
             type={props.viewed && !renderProps.hidden ? 'text' : 'password'}
             value={props.value}
             name={props.name}
-            autoComplete={props.autoComplete ? 'on' : 'off'}
+            autoComplete={props.autoComplete}
             autoFocus={props.autoFocus}
             placeholder={props.placeholder}
-            disabled={!!props.disabled}
+            disabled={props.disabled}
+            padded={!!props.hint}
+            error={!!props.error}
+            focused={renderProps.focused}
             maxLength={props.maxLength}
             mask={props.mask}
             pipe={props.pipe}
-            guide={false}
-            keepCharPositions={!!props.pipe}
-            placeholderChar={'\u2000'}
-            onChange={renderProps.onChange}
-            onFocus={renderProps.onFocus}
-            onBlur={renderProps.onBlur}
-            onKeyDown={renderProps.onKeyDown}
-            onKeyUp={renderProps.onKeyUp}
-          />
-        ) : (
-          <Input
-            width={1}
-            height={7}
-            m={0}
-            p={0}
-            pr={props.hint ? 7 : undefined}
-            r={0}
-            b="none"
-            bb={props.disabled ? '1px dotted #999' : props.error ? '2px solid #d0021b' : renderProps.focused ? '2px solid #ff8c00' : '1px solid rgba(0, 0, 0, 0.2)'}
-            size={5}
-            weight={300}
-            color={props.disabled ? '#666' : '#000'}
-            placeholderColor="#666"
-            cursor={props.disabled ? 'not-allowed' : 'text'}
-            bg="transparent"
-            transition="all 100ms cubic-bezier(0.4, 0.0, 0.2, 1)"
-            type={props.viewed && !renderProps.hidden ? 'text' : 'password'}
-            value={props.value}
-            name={props.name}
-            autoComplete={props.autoComplete ? 'on' : 'off'}
-            autoFocus={props.autoFocus}
-            placeholder={props.placeholder}
-            disabled={!!props.disabled}
-            maxLength={props.maxLength}
             onChange={renderProps.onChange}
             onFocus={renderProps.onFocus}
             onBlur={renderProps.onBlur}
