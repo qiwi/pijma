63722e1ec67d8c70856827e1aaab4905c67be933
a.nenashev <qq89276505884>
2018-12-05 14:33:56 +0300

    feat: accent brand button gradient

--- packages/desktop/src/button/Button.tsx
+++ packages/desktop/src/button/Button.tsx
@@ -26,6 +26,8 @@ const buttonHoverBackground: { [kind in ButtonProps['kind']]: string } = {
   simple: '#fff padding-box',
 }
 
+const accentButtonBackground: string = 'linear-gradient(to bottom, #ff9810, #ff8300)'
+
 const buttonBorder: { [kind in ButtonProps['kind']]: string } = {
   brand: 'none',
   simple: '1px solid rgba(0, 0, 0, 0.14)',
@@ -92,11 +94,11 @@ const Button: FunctionComponent<ButtonProps> = (props) => (
         width={!props.icon || props.text ? 1 : buttonSize[props.size]}
         height={buttonSize[props.size]}
         minWidth={buttonMinWith[props.size]}
-        bg={props.disabled ? '#e6e6e6' : renderProps.hover || renderProps.focus ? buttonHoverBackground[props.kind] : buttonBackground[props.kind]}
+        bg={props.disabled ? '#e6e6e6' : renderProps.hover || renderProps.focus ? buttonHoverBackground[props.kind] : props.size === 'accent' && props.kind === 'brand' ? accentButtonBackground : buttonBackground[props.kind]}
         b={props.disabled ? 'none' : renderProps.hover || renderProps.focus ? buttonHoverBorder[props.kind] : buttonBorder[props.kind]}
         r={buttonRadius[props.size]}
         s={props.disabled ? 'none' : renderProps.hover || renderProps.focus ? hoverShadow[props.kind] : props.size === 'accent' ? shadow[props.kind] : 'none'}
-        transition="all 300ms cubic-bezier(0.4, 0.0, 0.2, 1)"
+        transition="box-shadow 300ms cubic-bezier(0.4, 0.0, 0.2, 1)"
         onClick={renderProps.onClick}
         onFocus={renderProps.onFocus}
         onBlur={renderProps.onFocus}
