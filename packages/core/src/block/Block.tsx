7f7d8da7605d7e221d154ade95d4cb49e9d8b29f
a.nenashev <qq89276505884>
2018-12-12 17:00:28 +0300

    fix: remove overflow, added bg prop

--- packages/core/src/block/Block.tsx
+++ packages/core/src/block/Block.tsx
@@ -8,6 +8,7 @@ export interface BlockProps {
   focus?: boolean
   tabIndex?: number
   accent?: boolean
+  bg?: string
 }
 
 export const Block: FC<BlockProps> = (props) => {
@@ -18,7 +19,7 @@ export const Block: FC<BlockProps> = (props) => {
       s={props.hover || props.focus || props.active ? '0 10px 24px 0 rgba(0,0,0,0.08)' : '0 1px 2px 0 rgba(0,0,0,0.12)'}
       transition="all 300ms cubic-bezier(0.4, 0.0, 0.2, 1)"
       transform={props.hover && props.accent ? 'translateY(-4px)' : undefined}
-      overflow="hidden"
+      bg={props.bg ? props.bg : '#fff'}
       children={props.children}
     />
   )
