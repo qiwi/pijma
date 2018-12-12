f2cd122f55867244bd68b8a029be24c74ba4b466
a.nenashev <qq89276505884>
2018-12-12 14:45:31 +0300

    fix: transform

--- packages/core/src/block/Block.tsx
+++ packages/core/src/block/Block.tsx
@@ -17,7 +17,7 @@ export const Block: FC<BlockProps> = (props) => {
       tabIndex={props.tabIndex}
       s={props.hover || props.focus || props.active ? "0 10px 24px 0 rgba(0,0,0,0.08)" : "0 1px 2px 0 rgba(0,0,0,0.12)"}
       transition="all 300ms cubic-bezier(0.4, 0.0, 0.2, 1)"
-      css={props.hover && props.accent ? {transform: "translateY(-4px)"} : undefined}
+      transform={props.hover && props.accent ? "translateY(-4px)" : undefined}
       overflow="hidden"
       children={props.children}
     />
