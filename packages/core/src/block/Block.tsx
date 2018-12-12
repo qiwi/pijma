de6d0bb969828b50ef80016f78cee7a90551dd20
a.nenashev <qq89276505884>
2018-12-12 14:06:09 +0300

    feat: transition all

--- packages/core/src/block/Block.tsx
+++ packages/core/src/block/Block.tsx
@@ -17,7 +17,7 @@ export const Block: FC<BlockProps> = (props) => {
       r={10}
       tabIndex={props.tabIndex}
       s={props.hover || props.focus || props.active ? "0 10px 24px 0 rgba(0,0,0,0.08)" : "0 1px 2px 0 rgba(0,0,0,0.12)"}
-      transition="box-shadow 300ms cubic-bezier(0.4, 0.0, 0.2, 1)"
+      transition="all 300ms cubic-bezier(0.4, 0.0, 0.2, 1)"
       css={props.hover && props.accent ? {transform: 'translateY(-4px)'} : undefined}
       overflow='hidden'
       children={props.children}
