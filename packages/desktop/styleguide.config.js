8916ec9447cf23ce5f9060599cfca37e9604e31b
Dima Livshits <dimonka83@gmail.com>
2018-12-26 15:55:54 +0300

    chore: do not generate declarations for rsg

--- packages/desktop/styleguide.config.js
+++ packages/desktop/styleguide.config.js
@@ -75,7 +75,10 @@ module.exports = {
           test: /\.tsx?$/,
           use: [
             {
-              loader: 'ts-loader'
+              loader: 'ts-loader',
+              options: {
+                configFile: path.resolve(__dirname, 'tsconfig.json')
+              }
             }
           ],
           exclude: /node_modules/,
