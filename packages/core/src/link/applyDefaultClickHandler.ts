cbf2b7ef4e25e0830292de9c986417479de7a03a
a.nenashev <qq89276505884>
2018-12-27 13:03:55 +0300

    fix: import

--- packages/core/src/link/applyDefaultClickHandler.ts
+++ packages/core/src/link/applyDefaultClickHandler.ts
@@ -1,5 +1,5 @@
 import {LinkControl} from '@qiwi/pijma-core'
-import LinkControlProps from '@qiwi/pijma-core/link/LinkControlProps'
+import LinkControlProps from './LinkControlProps'
 
 export const applyDefaultClickHandler = (onClick: LinkControlProps['onClick']) => {
   LinkControl.defaultProps.onClick = onClick
