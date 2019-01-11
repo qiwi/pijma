ac4ae95fe0f73c961deb1424315f9cef72d83523
Anton Kharitonov <toxa85@gmail.com>
2019-01-11 15:56:57 +0300

    add placeholders for Text component

--- packages/desktop/src/typography/Text.tsx
+++ packages/desktop/src/typography/Text.tsx
@@ -1,6 +1,6 @@
 import React, {FunctionComponent} from 'react'
 
-import {Typo, TypoProps} from '@qiwi/pijma-core'
+import {Typo, TypoProps, Placeholder, Box} from '@qiwi/pijma-core'
 
 export interface TextProps {
   display?: 'block' | 'inline' | 'inline-block'
@@ -11,6 +11,7 @@ export interface TextProps {
   decoration?: TypoProps['decoration']
   transition?: TypoProps['transition']
   transform?: TypoProps['transform']
+  placeholder?: boolean
 }
 
 const TextSize: { [size in NonNullable<TextProps['size']>]: number } = {
@@ -31,6 +32,24 @@ const TextHeightCompact: { [size in NonNullable<TextProps['size']>]: number } = 
   l: 7,
 }
 
+const PlaceholderOffset: { [size in NonNullable<TextProps['size']>]: {top: number, bottom: number} } = {
+  s: {top: 2, bottom: 1},
+  m: {top: 1, bottom: 2},
+  l: {top: 3, bottom: 2},
+}
+
+const PlaceholderOffsetCompact: { [size in NonNullable<TextProps['size']>]: {top: number, bottom: number} } = {
+  s: {top: 1, bottom: 1},
+  m: {top: 1, bottom: 1},
+  l: {top: 3, bottom: 1},
+}
+
+const PlaceholderHeight: { [size in NonNullable<TextProps['size']>]: number } = {
+  s: 2,
+  m: 3,
+  l: 3,
+}
+
 const TextColor: { [color in NonNullable<TextProps['color']>]: string } = {
   default: '#000',
   support: '#666',
@@ -40,17 +59,29 @@ const TextColor: { [color in NonNullable<TextProps['color']>]: string } = {
   warning: '#ff8c00',
 }
 
-export const Text: FunctionComponent<TextProps> = ({display, compact, size, bold, color, decoration, transform, transition, children}) => (
-  <Typo
-    as="span"
-    display={display}
-    size={size === undefined ? undefined : TextSize[size]}
-    height={size === undefined ? undefined : compact ? TextHeightCompact[size] : TextHeight[size]}
-    weight={bold === undefined ? undefined : bold ? 500 : 300}
-    color={color === undefined ? undefined : TextColor[color]}
-    decoration={decoration}
-    transform={transform}
-    transition={transition}
-    children={children}
-  />
-)
+export const Text: FunctionComponent<TextProps> = ({display, compact, size, bold, color, decoration, transform, transition, placeholder, children}) => {
+  if (placeholder) {
+    const placeholderSize = size || 'm'
+    const offset = compact ? PlaceholderOffsetCompact[placeholderSize] : PlaceholderOffset[placeholderSize]
+    const placeholderColor = color === 'inverse' ? TextColor.inverse : TextColor.default
+    return (
+      <Box pt={offset.top} pb={offset.bottom}>
+        <Placeholder height={PlaceholderHeight[placeholderSize]} width={75} color={placeholderColor}/>
+      </Box>
+    )
+  }
+  return (
+    <Typo
+      as="span"
+      display={display}
+      size={size === undefined ? undefined : TextSize[size]}
+      height={size === undefined ? undefined : compact ? TextHeightCompact[size] : TextHeight[size]}
+      weight={bold === undefined ? undefined : bold ? 500 : 300}
+      color={color === undefined ? undefined : TextColor[color]}
+      decoration={decoration}
+      transform={transform}
+      transition={transition}
+      children={children}
+    />
+  )
+}
--- packages/mobile/src/typography/Text.tsx
+++ packages/mobile/src/typography/Text.tsx
@@ -1,6 +1,6 @@
 import React, {FunctionComponent} from 'react'
 
-import {Typo, TypoProps} from '@qiwi/pijma-core'
+import {Typo, TypoProps, Placeholder, Box} from '@qiwi/pijma-core'
 
 export interface TextProps {
   display?: 'block' | 'inline' | 'inline-block'
@@ -9,6 +9,7 @@ export interface TextProps {
   compact?: boolean
   color?: 'default' | 'support' | 'inverse' | 'failure' | 'success' | 'warning'
   transform?: TypoProps['transform']
+  placeholder?: boolean
 }
 
 const TextSize: { [size in NonNullable<TextProps['size']>]: number } = {
@@ -29,6 +30,24 @@ const TextHeightCompact: { [size in NonNullable<TextProps['size']>]: number } = 
   l: 7,
 }
 
+const PlaceholderOffset: { [size in NonNullable<TextProps['size']>]: {top: number, bottom: number} } = {
+  s: {top: 2, bottom: 1},
+  m: {top: 1, bottom: 2},
+  l: {top: 3, bottom: 2},
+}
+
+const PlaceholderOffsetCompact: { [size in NonNullable<TextProps['size']>]: {top: number, bottom: number} } = {
+  s: {top: 1, bottom: 1},
+  m: {top: 1, bottom: 1},
+  l: {top: 3, bottom: 1},
+}
+
+const PlaceholderHeight: { [size in NonNullable<TextProps['size']>]: number } = {
+  s: 2,
+  m: 3,
+  l: 3,
+}
+
 const TextColor: { [color in NonNullable<TextProps['color']>]: string } = {
   default: '#000',
   support: '#666',
@@ -38,15 +57,27 @@ const TextColor: { [color in NonNullable<TextProps['color']>]: string } = {
   warning: '#ff8c00',
 }
 
-export const Text: FunctionComponent<TextProps> = ({display, compact, size, bold, color, transform, children}) => (
-  <Typo
-    as="span"
-    display={display}
-    size={size === undefined ? undefined : TextSize[size]}
-    height={size === undefined ? undefined : compact ? TextHeightCompact[size] : TextHeight[size]}
-    weight={bold === undefined ? undefined : bold ? 500 : 300}
-    color={color === undefined ? undefined : TextColor[color]}
-    transform={transform}
-    children={children}
-  />
-)
+export const Text: FunctionComponent<TextProps> = ({display, compact, size, bold, color, transform, placeholder, children}) => {
+  if (placeholder) {
+    const placeholderSize = size || 'm'
+    const offset = compact ? PlaceholderOffsetCompact[placeholderSize] : PlaceholderOffset[placeholderSize]
+    const placeholderColor = color === 'inverse' ? TextColor.inverse : TextColor.default
+    return (
+      <Box pt={offset.top} pb={offset.bottom}>
+        <Placeholder height={PlaceholderHeight[placeholderSize]} width={75} color={placeholderColor}/>
+      </Box>
+    )
+  }
+  return (
+    <Typo
+      as="span"
+      display={display}
+      size={size === undefined ? undefined : TextSize[size]}
+      height={size === undefined ? undefined : compact ? TextHeightCompact[size] : TextHeight[size]}
+      weight={bold === undefined ? undefined : bold ? 500 : 300}
+      color={color === undefined ? undefined : TextColor[color]}
+      transform={transform}
+      children={children}
+    />
+  )
+}
