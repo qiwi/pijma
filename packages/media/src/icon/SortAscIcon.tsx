73b7d3a860c9801f651f761e829f126e11c4d6f0
Vasilii Lantratov <lantratov.vasia@yandex.ru>
2019-01-17 18:19:53 +0300

    add sort icons (asc/desc)

--- packages/media/src/icon/SortAscIcon.tsx
+++ packages/media/src/icon/SortAscIcon.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const SortAscIcon: React.FunctionComponent = () => (
-  <svg viewBox='0 0 24 24' focusable="false">
-    <g fillRule='evenodd' transform='matrix(1 0 0 -1 0 24)'>
-      <path d='M21 5H3v2h18V5zm-4 6H3v2h14v-2zM3 19h10v-2H3v2z' />
+  <svg viewBox="0 0 24 24" focusable="false">
+    <g fillRule="evenodd" transform="matrix(1 0 0 -1 0 24)">
+      <path d="M21 5H3v2h18V5zm-4 6H3v2h14v-2zM3 19h10v-2H3v2z" />
     </g>
   </svg>
 )
