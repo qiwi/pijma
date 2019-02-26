a6a9710aded5a4c462baccd1cb2ddfaae226e6ec
Second To None <secondtonone.secondtonone@gmail.com>
2019-02-26 15:26:49 +0300

    Deleting Textarea.ts

--- packages/core/src/primitive/Textarea.ts
+++
@@ -1,8 +0,0 @@
-import styled from '../styled'
-
-import {Input, InputProps} from './Input'
-
-export const TextArea = styled(Input.withComponent('textarea'))<InputProps>(() => ({
-  resize: 'none',
-  overflowY: 'auto'
-}))
