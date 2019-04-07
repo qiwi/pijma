6ce62e72ad51da2f830ea6f94df7f21622618f15
Second To None <secondtonone.secondtonone@gmail.com>
2019-04-07 23:36:51 +0300

    Change resize handling

--- packages/core/src/text-area-field/TextAreaFieldControl.tsx
+++ packages/core/src/text-area-field/TextAreaFieldControl.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import TextAreaFieldControlProps from './TextAreaFieldControlProps'
-import TextAreaFieldControlState from './TextAreaFieldControlState'
+import TextAreaFieldControlProps from './TextAreaFieldControlProps';
+import TextAreaFieldControlState from './TextAreaFieldControlState';
 
 export default class TextAreaFieldControl extends React.Component<
   TextAreaFieldControlProps,
@@ -11,81 +11,86 @@ export default class TextAreaFieldControl extends React.Component<
   state = {
     focused: false,
     rows: 1
-  }
+  };
 
   public componentDidMount(): void {
-    this.resize()
+    this.resize();
+  }
+
+  public componentDidUpdate(prevProps: TextAreaFieldControlProps): void {
+    if (this.props.value !== prevProps.value) {
+      this.resize();
+    }
   }
 
   private resize(): void {
     const element = (ReactDOM.findDOMNode(this) as HTMLElement).querySelector(
       'textarea'
-    ) as HTMLTextAreaElement
+    ) as HTMLTextAreaElement;
 
-    element.style.height = '0px'
+    element.style.height = '0px';
 
-    const style = getComputedStyle(element)
-    const lineHeight = parseInt(style.lineHeight || '0', 10)
-    const scrollHeight = element.scrollHeight
+    const style = getComputedStyle(element);
+    const lineHeight = parseInt(style.lineHeight || '0', 10);
+    const scrollHeight = element.scrollHeight;
 
-    element.style.height = 'auto'
+    element.style.height = 'auto';
 
-    const rows = Math.ceil(scrollHeight / lineHeight)
+    const rows = Math.ceil(scrollHeight / lineHeight);
 
     this.setState({
       rows
-    })
+    });
   }
 
   private onChange: React.ChangeEventHandler<HTMLTextAreaElement> = (
     e: React.ChangeEvent<HTMLTextAreaElement>
   ) => {
-    e.preventDefault()
+    e.preventDefault();
     if (this.props.onChange) {
-      this.props.onChange(e.currentTarget.value)
+      this.props.onChange(e.currentTarget.value);
     }
-  }
+  };
 
   private onFocus: React.FocusEventHandler<HTMLTextAreaElement> = (
     event: React.FocusEvent<HTMLTextAreaElement>
   ) => {
     this.setState({
       focused: true
-    })
-    event.preventDefault()
+    });
+    event.preventDefault();
     if (this.props.onFocus) {
-      this.props.onFocus()
+      this.props.onFocus();
     }
-  }
+  };
 
   private onBlur: React.FocusEventHandler<HTMLTextAreaElement> = (
     e: React.FocusEvent<HTMLTextAreaElement>
   ) => {
     this.setState({
       focused: false
-    })
-    e.preventDefault()
+    });
+    e.preventDefault();
     if (this.props.onBlur) {
-      this.props.onBlur()
+      this.props.onBlur();
     }
-  }
+  };
 
   private onKeyDown: React.KeyboardEventHandler<HTMLTextAreaElement> = (
     event: React.KeyboardEvent<HTMLTextAreaElement>
   ) => {
-    this.resize()
     if (this.props.onKeyDown && this.props.onKeyDown(event)) {
-      event.preventDefault()
+      event.preventDefault();
     }
-  }
+  };
 
   private onKeyUp: React.KeyboardEventHandler = (
     event: React.KeyboardEvent
   ) => {
     if (this.props.onKeyUp && this.props.onKeyUp(event)) {
-      event.preventDefault()
+      event.preventDefault();
     }
-  }
+  };
 
   public render() {
     return this.props.children({
@@ -96,6 +101,6 @@ export default class TextAreaFieldControl extends React.Component<
       onBlur: this.onBlur,
       onKeyDown: this.onKeyDown,
       onKeyUp: this.onKeyUp
-    })
+    });
   }
 }
