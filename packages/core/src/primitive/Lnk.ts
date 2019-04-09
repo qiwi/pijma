f32c05de2d10902eb65b6487a2c625ab210ea18c
a.nenashev <a.nenashev@qiwi.com>
2019-04-09 16:16:14 +0300

    Props removed

--- packages/core/src/primitive/Lnk.ts
+++ packages/core/src/primitive/Lnk.ts
@@ -1,15 +1,7 @@
 import styled from '../styled'
 
-export interface LnkProps {
-  textDecoration?: string
-  cursor?: string
-}
-
-export const LnkNonProps = ['textDecoration', 'cursor']
-
 export const Lnk = styled('a', {
-  shouldForwardProp: (prop) => !LnkNonProps.includes(prop),
-})<LnkProps>(()=>({
+})(()=>({
   textDecoration: 'none',
   cursor: 'pointer',
 }))
