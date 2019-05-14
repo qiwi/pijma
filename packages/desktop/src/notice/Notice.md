5e8404a8a5945366f130b13f6c9c8b8adab0af21
Nikita Savkin <n.savkin@qiwi.com>
2019-05-14 19:44:27 +0300

    [NF] can use IconName for icon

--- packages/desktop/src/notice/Notice.md
+++ packages/desktop/src/notice/Notice.md
@@ -1,13 +1,9 @@
-## Состояния блоков
-
-#### Default
-
 ```jsx
 <Card bg="#eee" p={7}>
   <Block>
     <Card p={7}>
       <Notice 
-        icon={<Icon name="attention"/>}
+        icon="attention"
         children="Заголовок какого-нибудь алерта \n Заголовок какого-нибудь алерта"
       />
     </Card>
--- packages/desktop/src/notice/Notice.tsx
+++ packages/desktop/src/notice/Notice.tsx
@@ -1,20 +1,27 @@
 import React, {FC, isValidElement, ReactNode} from 'react'
-import {Flex, FlexItem, Spacer} from '@qiwi/pijma-core'
+import {Flex, FlexItem, Icon, Spacer} from '@qiwi/pijma-core'
 import {Heading, Paragraph} from '@qiwi/pijma-desktop'
+import {IconName} from "@qiwi/pijma-media";
 
 export interface NoticeProps {
-  icon: ReactNode
+  icon: ReactNode | IconName
   title?: string | ReactNode
 }
 
 export const Notice: FC<NoticeProps> = ({icon, title, children}) => (
   <Flex>
-    <FlexItem
-      shrink={0}
-      basis={6}
-      mr={2}
-      children={icon}
-    />
+    {typeof icon === 'string' ? (
+      <FlexItem
+        shrink={0}
+        basis={6}
+        mr={2}
+        children={<Icon name={icon as IconName}/>}
+      />
+    ) : isValidElement(icon) ? (
+      icon
+    ) : (
+      null
+    )}
     {title || children ? (
       <FlexItem>
         <Spacer size="xs">
