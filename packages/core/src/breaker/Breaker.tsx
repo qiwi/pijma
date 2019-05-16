1426846324a07366e9fa95876e056a59b18997fa
Nikita Savkin <n.savkin@qiwi.com>
2019-05-16 18:59:45 +0300

    feat: added breaker component

---
+++ packages/core/src/breaker/Breaker.tsx
@@ -0,0 +1,17 @@
+import React, {FC, Fragment} from 'react'
+
+const newLineRegex = /(\r\n|\r|\n)/g
+
+export const Breaker: FC = ({children}) => (
+  <Fragment>
+    {typeof children === 'string' ? (
+      children.split(newLineRegex).map((child, key) => child.match(newLineRegex) ? (
+        <br key={key}/>
+      ) : (
+        child
+      ))
+    ) : (
+      children
+    )}
+  </Fragment>
+)
