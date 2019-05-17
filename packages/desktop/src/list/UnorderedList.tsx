d7cb8498f2c9077d8b570f8c84602478df3531ed
Anastasia Gorshkova <anastasia.gorshkova@simbirsoft.com>
2019-05-17 21:21:34 +0400

    feat: add unordered items

---
+++ packages/desktop/src/list/UnorderedList.tsx
@@ -0,0 +1,40 @@
+import React, {FunctionComponent, ReactNode} from 'react'
+
+import {Box, Flex, Card} from '@qiwi/pijma-core'
+import {Paragraph} from '../typography'
+
+export interface UnorderedListProps<I> {
+  items: I[]
+}
+
+const BoxUl = Flex.withComponent('ul')
+const FlexLi = Box.withComponent('li')
+
+export const UnorderedList: FunctionComponent<
+  UnorderedListProps<ReactNode>
+> = props => (
+  <BoxUl>
+    {props.items.map((item, index) => (
+      <FlexLi key={index} mb={index + 1 === props.items.length ? undefined : 4} display="flex">
+        <Card
+          mr={3}
+          my="9px"
+          display="inline-block"
+          width="6px"
+          minWidth="6px"
+          height="6px"
+          bg="#000"
+          r="50%"
+          css={{verticalAlign: 'middle'}}
+        />
+        <Box display="inline-block" css={{verticalAlign: 'middle'}}>
+          {typeof item === 'string' ? (
+            <Paragraph size="m">{item}</Paragraph>
+          ) : (
+            item
+          )}
+        </Box>
+      </FlexLi>
+    ))}
+  </BoxUl>
+)
