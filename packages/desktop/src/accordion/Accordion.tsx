70e13a66eca4cc8ec2563c21fd483faa11f35bcf
Anastasia Gorshkova <anastasia.gorshkova@simbirsoft.com>
2019-05-17 14:23:55 +0400

    fix: remove border for accordion items

--- packages/desktop/src/accordion/Accordion.tsx
+++ packages/desktop/src/accordion/Accordion.tsx
@@ -6,6 +6,7 @@ import {Paragraph} from '../typography'
 
 export interface AccordionProps<I> {
   items: I[]
+  bg: string
 }
 
 export interface AccordionItemModel {
@@ -19,59 +20,55 @@ const Accordion: FunctionComponent<
   <AccordionControl<AccordionItemModel>
     items={props.items}
     children={renderProps => (
-      <Card r={10} bg="#fff" s="0 1px 2px 0 rgba(0, 0, 0, 0.12)" py={3}>
-        <Box onMouseLeave={renderProps.onMouseLeave}>
-          {renderProps.items.map((item, index) => (
-            <Card
-              key={index}
-              onMouseEnter={item.onMouseEnter}
-              bb={
-                index + 1 === renderProps.items.length
-                  ? ''
-                  : item.hovered
-                  ? '1px solid transparent'
-                  : '1px solid #e6e6e6'
-              }
-              bt={item.hovered ? '1px solid white' : ''}
-              mt={item.hovered ? '-1px' : 0}
-              s={item.hovered ? '0 0 16px 0 rgba(0, 0, 0, 0.12)' : undefined}
-              transition="box-shadow 3ms, border-bottom-color 100ms"
+      <Box onMouseLeave={renderProps.onMouseLeave}>
+        {renderProps.items.map((item, index) => (
+          <Card
+            key={index}
+            onMouseEnter={item.onMouseEnter}
+            bg={item.hovered ? props.bg : undefined}
+            s={
+              item.hovered
+                ? '0 0 16px 0 rgba(0, 0, 0, 0.12)'
+                : index + 1 === renderProps.items.length
+                ? undefined
+                : '0 1px 0 #e6e6e6'
+            }
+            transition="box-shadow 3ms, border-bottom-color 100ms"
+          >
+            <Flex
+              onClick={item.onClick}
+              wrap="nowrap"
+              justify="space-between"
+              align="start"
+              cursor="pointer"
+              px={8}
+              pt={4}
+              pb={item.opened ? 2 : 4}
             >
-              <Flex
-                onClick={item.onClick}
-                wrap="nowrap"
-                justify="space-between"
-                align="start"
-                cursor="pointer"
-                px={8}
-                pt={4}
-                pb={item.opened ? 2 : 4}
-              >
-                <Paragraph bold size="m">
-                  {item.title}
-                </Paragraph>
-                <FlexItem shrink={0} width={6} height={6} ml={3}>
-                  <Box
-                    transform={`rotate(${item.opened ? 180 : 0}deg)`}
-                    transition="transform 0.3s ease-in-out"
-                  >
-                    <Icon name="angle-small-down" />
-                  </Box>
-                </FlexItem>
-              </Flex>
-              {item.opened ? (
-                <Box px={8} pb={4}>
-                  {typeof item.content === 'string' ? (
-                    <Paragraph size="m">{item.content}</Paragraph>
-                  ) : (
-                    item.content
-                  )}
+              <Paragraph bold size="m">
+                {item.title}
+              </Paragraph>
+              <FlexItem shrink={0} width={6} height={6} ml={3}>
+                <Box
+                  transform={`rotate(${item.opened ? 180 : 0}deg)`}
+                  transition="transform 0.3s ease-in-out"
+                >
+                  <Icon name="angle-small-down" />
                 </Box>
-              ) : null}
-            </Card>
-          ))}
-        </Box>
-      </Card>
+              </FlexItem>
+            </Flex>
+            {item.opened ? (
+              <Box px={8} pb={4}>
+                {typeof item.content === 'string' ? (
+                  <Paragraph size="m">{item.content}</Paragraph>
+                ) : (
+                  item.content
+                )}
+              </Box>
+            ) : null}
+          </Card>
+        ))}
+      </Box>
     )}
   />
 )
--- packages/mobile/src/accordion/Accordion.tsx
+++ packages/mobile/src/accordion/Accordion.tsx
@@ -11,6 +11,7 @@ export interface AccordionProps<I> {
 export interface AccordionItemModel {
   title: string
   content: ReactNode
+  bg: string
 }
 
 const Accordion: FunctionComponent<
@@ -19,54 +20,52 @@ const Accordion: FunctionComponent<
   <AccordionControl<AccordionItemModel>
     items={props.items}
     children={renderProps => (
-      <Card r={10} bg="#fff" s="0 1px 2px 0 rgba(0, 0, 0, 0.12)" py={2}>
-        <Box onMouseLeave={renderProps.onMouseLeave}>
-          {renderProps.items.map((item, index) => (
-            <Card
-              key={index}
-              onMouseEnter={item.onMouseEnter}
-              bb={
-                index + 1 === renderProps.items.length
-                  ? ''
-                  : '1px solid #e6e6e6'
-              }
-              transition="box-shadow 3ms, border-bottom-color 100ms"
+      <Box onMouseLeave={renderProps.onMouseLeave}>
+        {renderProps.items.map((item, index) => (
+          <Card
+            key={index}
+            onMouseEnter={item.onMouseEnter}
+            s={
+              index + 1 === renderProps.items.length
+                ? undefined
+                : '0 1px 0 #e6e6e6'
+            }
+            transition="box-shadow 3ms, border-bottom-color 100ms"
+          >
+            <Flex
+              onClick={item.onClick}
+              wrap="nowrap"
+              justify="space-between"
+              align="start"
+              cursor="pointer"
+              px={4}
+              pt={4}
+              pb={item.opened ? 1 : 4}
             >
-              <Flex
-                onClick={item.onClick}
-                wrap="nowrap"
-                justify="space-between"
-                align="start"
-                cursor="pointer"
-                px={4}
-                pt={4}
-                pb={item.opened ? 1 : 4}
-              >
-                <Paragraph bold size="s">
-                  {item.title}
-                </Paragraph>
-                <FlexItem shrink={0} width={6} height={6} ml={3}>
-                  <Box
-                    transform={`rotate(${item.opened ? 180 : 0}deg)`}
-                    transition="transform 0.3s ease-in-out"
-                  >
-                    <Icon name="angle-small-down" />
-                  </Box>
-                </FlexItem>
-              </Flex>
-              {item.opened ? (
-                <Box px={4} pb={4}>
-                  {typeof item.content === 'string' ? (
-                    <Paragraph size="s">{item.content}</Paragraph>
-                  ) : (
-                    item.content
-                  )}
+              <Paragraph bold size="s">
+                {item.title}
+              </Paragraph>
+              <FlexItem shrink={0} width={6} height={6} ml={3}>
+                <Box
+                  transform={`rotate(${item.opened ? 180 : 0}deg)`}
+                  transition="transform 0.3s ease-in-out"
+                >
+                  <Icon name="angle-small-down" />
                 </Box>
-              ) : null}
-            </Card>
-          ))}
-        </Box>
-      </Card>
+              </FlexItem>
+            </Flex>
+            {item.opened ? (
+              <Box px={4} pb={4}>
+                {typeof item.content === 'string' ? (
+                  <Paragraph size="s">{item.content}</Paragraph>
+                ) : (
+                  item.content
+                )}
+              </Box>
+            ) : null}
+          </Card>
+        ))}
+      </Box>
     )}
   />
 )
