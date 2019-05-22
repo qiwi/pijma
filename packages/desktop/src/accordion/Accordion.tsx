5c5b8c6e741f948632fa8406ce7852a422059797
Anastasia Gorshkova <anastasia.gorshkova@simbirsoft.com>
2019-05-23 01:21:29 +0400

    fix: remove wrapper box

--- packages/desktop/src/accordion/Accordion.tsx
+++ packages/desktop/src/accordion/Accordion.tsx
@@ -23,70 +23,63 @@ const AccordionIndent: {
   l: 17,
 }
 
-const Accordion: FunctionComponent<
+export const Accordion: FunctionComponent<
   AccordionProps<AccordionItemModel>
 > = ({items, bg = '#fff', indent = 'm'}) => (
   <AccordionControl<AccordionItemModel>
     items={items}
     children={renderProps => (
-      <Box py={3}>
-        <Box onMouseLeave={renderProps.onMouseLeave}>
-          {renderProps.items.map((item, index) => (
-            <Card
-              key={index}
-              bg={item.hovered ? bg : undefined}
-              s={
-                item.hovered
-                  ? '0 0 16px 0 rgba(0, 0, 0, 0.12)'
-                  : index + 1 === renderProps.items.length
-                  ? undefined
-                  : '0 1px 0 #e6e6e6'
-              }
-              transition="box-shadow 100ms cubic-bezier(0.4, 0.0, 0.2, 1)"
-              onMouseEnter={item.onMouseEnter}
+      <Box py={3} onMouseLeave={renderProps.onMouseLeave}>
+        {renderProps.items.map((item, index) => (
+          <Card
+            key={index}
+            bg={item.hovered ? bg : undefined}
+            s={
+              item.hovered
+                ? '0 0 16px 0 rgba(0, 0, 0, 0.12)'
+                : index + 1 === renderProps.items.length
+                ? undefined
+                : '0 1px 0 #e6e6e6'
+            }
+            transition="box-shadow 100ms cubic-bezier(0.4, 0.0, 0.2, 1)"
+            onMouseEnter={item.onMouseEnter}
+          >
+            <Flex
+              wrap="nowrap"
+              justify="space-between"
+              align="start"
+              cursor="pointer"
+              px={AccordionIndent[indent]}
+              pt={4}
+              pb={item.opened ? 2 : 4}
+              onClick={item.onClick}
             >
-              <Flex
-                wrap="nowrap"
-                justify="space-between"
-                align="start"
-                cursor="pointer"
-                px={AccordionIndent[indent]}
-                pt={4}
-                pb={item.opened ? 2 : 4}
-                onClick={item.onClick}
+              <Paragraph bold size="m">
+                {item.title}
+              </Paragraph>
+              <FlexItem
+                shrink={0}
+                width={6}
+                height={6}
+                ml={3}
+                transform={`rotate(${item.opened ? 180 : 0}deg)`}
+                transition="transform 0.3s ease-in-out"
               >
-                <Paragraph bold size="m">
-                  {item.title}
-                </Paragraph>
-                <FlexItem
-                  shrink={0}
-                  width={6}
-                  height={6}
-                  ml={3}
-                  transform={`rotate(${item.opened ? 180 : 0}deg)`}
-                  transition="transform 0.3s ease-in-out"
-                >
-                  <Icon name="angle-small-down" />
-                </FlexItem>
-              </Flex>
-              {item.opened ? (
-                <Box
-                  px={AccordionIndent[indent]}
-                  pb={4}
-                >
-                  {typeof item.content === 'string' ? (
-                    <Paragraph size="m">{item.content}</Paragraph>
-                  ) : (
-                    item.content
-                  )}
-                </Box>
-              ) : null}
-            </Card>
-          ))}
-        </Box>
+                <Icon name="angle-small-down" />
+              </FlexItem>
+            </Flex>
+            {item.opened ? (
+              <Box px={AccordionIndent[indent]} pb={4}>
+                {typeof item.content === 'string' ? (
+                  <Paragraph size="m">{item.content}</Paragraph>
+                ) : (
+                  item.content
+                )}
+              </Box>
+            ) : null}
+          </Card>
+        ))}
       </Box>
     )}
   />
 )
-
-export default Accordion
--- packages/mobile/src/accordion/Accordion.tsx
+++ packages/mobile/src/accordion/Accordion.tsx
@@ -13,64 +13,60 @@ export interface AccordionItemModel {
   content: ReactNode
 }
 
-const Accordion: FunctionComponent<
+export const Accordion: FunctionComponent<
   AccordionProps<AccordionItemModel>
 > = ({items}) => (
   <AccordionControl<AccordionItemModel>
     items={items}
     children={renderProps => (
-      <Box py={2}>
-        <Box onMouseLeave={renderProps.onMouseLeave}>
-          {renderProps.items.map((item, index) => (
-            <Card
-              key={index}
-              s={
-                index + 1 === renderProps.items.length
-                  ? undefined
-                  : '0 1px 0 #e6e6e6'
-              }
-              transition="box-shadow 100ms cubic-bezier(0.4, 0.0, 0.2, 1)"
-              onMouseEnter={item.onMouseEnter}
+      <Box onMouseLeave={renderProps.onMouseLeave} py={2}>
+        {renderProps.items.map((item, index) => (
+          <Card
+            key={index}
+            s={
+              index + 1 === renderProps.items.length
+                ? undefined
+                : '0 1px 0 #e6e6e6'
+            }
+            transition="box-shadow 100ms cubic-bezier(0.4, 0.0, 0.2, 1)"
+            onMouseEnter={item.onMouseEnter}
+          >
+            <Flex
+              wrap="nowrap"
+              justify="space-between"
+              align="start"
+              cursor="pointer"
+              px={4}
+              pt={4}
+              pb={item.opened ? 1 : 4}
+              onClick={item.onClick}
             >
-              <Flex
-                wrap="nowrap"
-                justify="space-between"
-                align="start"
-                cursor="pointer"
-                px={4}
-                pt={4}
-                pb={item.opened ? 1 : 4}
-                onClick={item.onClick}
+              <Paragraph bold size="s">
+                {item.title}
+              </Paragraph>
+              <FlexItem
+                shrink={0}
+                width={6}
+                height={6}
+                ml={3}
+                transform={`rotate(${item.opened ? 180 : 0}deg)`}
+                transition="transform 0.3s ease-in-out"
               >
-                <Paragraph bold size="s">
-                  {item.title}
-                </Paragraph>
-                <FlexItem
-                  shrink={0}
-                  width={6}
-                  height={6}
-                  ml={3}
-                  transform={`rotate(${item.opened ? 180 : 0}deg)`}
-                  transition="transform 0.3s ease-in-out"
-                >
-                  <Icon name="angle-small-down" />
-                </FlexItem>
-              </Flex>
-              {item.opened ? (
-                <Box px={4} pb={4}>
-                  {typeof item.content === 'string' ? (
-                    <Paragraph size="s">{item.content}</Paragraph>
-                  ) : (
-                    item.content
-                  )}
-                </Box>
-              ) : null}
-            </Card>
-          ))}
-        </Box>
+                <Icon name="angle-small-down" />
+              </FlexItem>
+            </Flex>
+            {item.opened ? (
+              <Box px={4} pb={4}>
+                {typeof item.content === 'string' ? (
+                  <Paragraph size="s">{item.content}</Paragraph>
+                ) : (
+                  item.content
+                )}
+              </Box>
+            ) : null}
+          </Card>
+        ))}
       </Box>
     )}
   />
 )
-
-export default Accordion
