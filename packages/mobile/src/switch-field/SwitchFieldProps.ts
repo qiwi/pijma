11190eab7c4288a51cd6b35bb41dc3002e0fda60
Maxim Kuznetsov <fixer40@bk.ru>
2019-05-29 17:27:32 +0300

    feat: add mobile switch

---
+++ packages/mobile/src/switch-field/SwitchFieldProps.ts
@@ -0,0 +1,18 @@
+import {ReactNode} from 'react'
+
+import {OptionModel} from '@qiwi/pijma-core'
+
+export default interface SwitchFieldProps<O extends OptionModel<V>, V> {
+  title?: ReactNode
+  hint?: ReactNode
+  help?: ReactNode
+  tabIndex?: number
+  autoFocus?: boolean
+  reverse?: boolean
+  options: O[]
+  values: V[]
+  equals?: (a: V, b: V) => boolean
+  onChange: (values: V[]) => void
+  onFocus?: () => void
+  onBlur?: () => void
+}
