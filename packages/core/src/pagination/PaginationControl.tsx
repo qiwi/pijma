2c21afa62f0edadf9f42c21aeb97216961dd86ac
Anastasia Gorshkova <anastasia.gorshkova@simbirsoft.com>
2019-05-29 17:03:34 +0400

    fix: calculations in paginationControl

--- packages/core/src/pagination/PaginationControl.tsx
+++ packages/core/src/pagination/PaginationControl.tsx
@@ -19,38 +19,23 @@ export class PaginationControl extends React.Component<PaginationControlProps> {
   private buildPages = () => {
     const {total, count, active} = this.props
     const totalPages = Math.max(Math.ceil(total), 1)
-    const length = Math.max(Math.ceil(count), 1)
+    const visiblePages = Math.max(Math.ceil(count), 1)
     const currentPage = Math.min(Math.max(Math.ceil(active), 1), totalPages)
 
-    let firstPage = Math.max(1, currentPage - Math.floor(length / 2))
-    let lastPage = Math.min(totalPages, currentPage + Math.floor(length / 2))
-
-    if (lastPage - firstPage + 1 < length) {
-      if (currentPage < totalPages / 2) {
-        lastPage = Math.min(
-          totalPages,
-          lastPage + (length - (lastPage - firstPage)),
-        )
-      }
-      else {
-        firstPage = Math.max(1, firstPage - (length - (lastPage - firstPage)))
-      }
-    }
-    if (lastPage - firstPage + 1 > length) {
-      if (currentPage > totalPages / 2) {
-        firstPage++
-      }
-      else {
-        lastPage--
-      }
-    }
+    const firstPage = Math.max(
+      1,
+      Math.min(
+        currentPage - Math.floor(visiblePages / 2),
+        totalPages - visiblePages + 1,
+      ),
+    )
 
     return {
       previousPage: currentPage - 1,
       nextPage: currentPage + 1,
       currentPage,
       totalPages,
-      pages: new Array(lastPage - firstPage + 1)
+      pages: new Array(Math.min(visiblePages, totalPages))
         .fill(0)
         .map((_, i) => firstPage + i),
     }
