a5a92ef754618b1416db5b66f4cff71f2be0641c
Anton Kharitonov <toxa85@gmail.com>
2019-05-29 10:33:39 +0300

    add striper

---
+++ packages/core/src/striper/Striper.tsx
@@ -0,0 +1,25 @@
+import React, {ReactNode, FunctionComponent, Children, Fragment} from 'react'
+
+import {Card} from '../primitive'
+
+export const Striper: FunctionComponent = ({children}) => {
+  const elements = Children.toArray(children).filter(child => !!child)
+  if (elements.length === 0) {
+    return null
+  }
+  return (
+    <Fragment>
+      {Children.map(elements, (child: ReactNode, key: number) => (
+        <Card
+          key={key}
+          children={child}
+          s={key === 0 ? 'none' : '0 -1px 0 0 #e6e6e6'}
+        />
+      ))}
+    </Fragment>
+  )
+}
+
+Striper.defaultProps = {
+  size: 'm',
+}
