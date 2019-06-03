0bb01e91d4d6a85d35ac7a92f343d961091c2f65
Nikita Savkin <n.savkin@qiwi.com>
2019-06-03 16:48:11 +0300

    fix: only IconName for icon

--- packages/mobile/src/notice/Notice.tsx
+++ packages/mobile/src/notice/Notice.tsx
@@ -1,32 +1,21 @@
-import React, {FC, isValidElement, ReactNode} from 'react'
+import React, {FC, ReactNode} from 'react'
 import {Flex, FlexItem, Icon, Spacer} from '@qiwi/pijma-core'
 import {IconName} from '@qiwi/pijma-media'
 import {Heading, Paragraph} from '../typography/'
 
 export interface NoticeProps {
-  icon: ReactNode
+  icon: IconName
   title?: ReactNode
 }
 
 export const Notice: FC<NoticeProps> = ({icon, title, children}) => (
   <Flex>
-    {typeof icon === 'string' ? (
-      <FlexItem
-        shrink={0}
-        basis={6}
-        mr={title ? 4 : 2}
-        children={<Icon name={icon as IconName}/>}
-      />
-    ) : isValidElement(icon) ? (
-      <FlexItem
-        shrink={0}
-        basis={6}
-        mr={title ? 4 : 2}
-        children={icon}
-      />
-    ) : (
-      null
-    )}
+    <FlexItem
+      shrink={0}
+      basis={6}
+      mr={title ? 4 : 2}
+      children={<Icon name={icon}/>}
+    />
     {title || children ? (
       <FlexItem>
         <Spacer size="xs">
