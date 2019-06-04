8b78bdf304ff4514fa53020b9ef755e7242290d8
Olesya Tenkova <olesya.tenkova@simbirsoft.com>
2019-06-04 16:51:56 +0400

    fix: no compact text

--- packages/desktop/src/list/List.tsx
+++ packages/desktop/src/list/List.tsx
@@ -33,33 +33,37 @@ export const List: FunctionComponent<ListProps> = props => (
       <Flex key={index} as="li" mt={index > 0 ? ItemIndent[props.type] : 0}>
         {props.type === 'number' ? (
           <FlexItem width={String(props.children.length).length * 2 + 3} shrink={0}>
-            <Text size="m" bold={false} compact>{index + 1}.</Text>
+            <Text size="m" bold={false}>{index + 1}.</Text>
           </FlexItem>
         ) : props.type === 'bullet' ? (
           <FlexItem width={5} shrink={0}>
-            <Text size="m" bold={false} compact>&#8226;</Text>
+            <Text size="m" bold={false}>&#8226;</Text>
           </FlexItem>
         ) : props.type === 'step' ? (
           <Flex direction="column" height="auto" mr={5}>
             <FlexItem shrink={0}>
               <Card bg="#F5F5F5" r="50%" height={10} width={10}>
                 <Flex align="center" justify="center" height={1} width={1}>
-                  <Text size="m" bold compact>
+                  <Text size="m" bold>
                     {index + 1}
                   </Text>
                 </Flex>
               </Card>
             </FlexItem>
-            {index + 1 === props.children.length ? null : (
+            {index + 1 === props.children.length ? (
+              null
+            ) : (
               <FlexItem height={1} minHeight={3} align="center" justify="center" mt={3}>
                 <Card bg="#F5F5F5" height={1} width="4px"/>
               </FlexItem>
             )}
           </Flex>
-        ) : null}
+        ) : (
+          null
+        )}
         <Box my={ListItemYMargin[props.type]}>
           {typeof item === 'string' ? (
-            <Paragraph size="m" compact>{item}</Paragraph>
+            <Paragraph size="m">{item}</Paragraph>
           ) : (
             item
           )}
