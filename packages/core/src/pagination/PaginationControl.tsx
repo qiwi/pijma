ff4f20c83a5cf5805f79e300eac730726bf3b7f5
Olesya Tenkova <olesya.tenkova@simbirsoft.com>
2019-06-20 11:14:05 +0400

    fix: paginatiomControl refucktoring

--- packages/core/src/pagination/PaginationControl.tsx
+++ packages/core/src/pagination/PaginationControl.tsx
@@ -20,8 +20,14 @@ export interface PaginationControlProps {
 
 export class PaginationControl extends React.Component<PaginationControlProps> {
 
-  private buildPages = () => {
-    const {total, count, active, href, onChange, onClick} = this.props
+  private onPageClick = (page: number, disabled: boolean) => this.props.href
+    ? this.props.onClick
+    : !disabled && this.props.onChange && this.props.onChange(page)
+
+  public render() {
+    const {total, count, active} = this.props
+    const isValidProps: boolean = active <= total && count <= total
+
     const totalPages = Math.max(Math.ceil(total), 1)
     const visiblePages = Math.max(Math.ceil(count), 1)
     const currentPage = Math.min(Math.max(Math.ceil(active), 1), totalPages)
@@ -34,24 +40,18 @@ export class PaginationControl extends React.Component<PaginationControlProps> {
       ),
     )
 
-    return {
-      previousPage: currentPage - 1,
-      nextPage: currentPage + 1,
-      currentPage,
-      totalPages,
-      pages: new Array(Math.min(visiblePages, totalPages))
-        .fill(0)
-        .map((_, i) => firstPage + i),
-      onPageClick: (page: number, disabled: boolean) => href
-        ? onClick
-        : !disabled && onChange && onChange(page),
-    }
-  }
-  public render() {
-    const {total, count, active} = this.props
-    const isValidProps: boolean = active <= total && count <= total
-
-    return isValidProps ? this.props.children(this.buildPages()) : null
+    return isValidProps
+      ? this.props.children({
+        previousPage: currentPage - 1,
+        nextPage: currentPage + 1,
+        currentPage,
+        totalPages,
+        pages: new Array(Math.min(visiblePages, totalPages))
+          .fill(0)
+          .map((_, i) => firstPage + i),
+        onPageClick: this.onPageClick,
+      })
+      : null
   }
 
 }
