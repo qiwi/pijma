603ec0dc64fa6411ffacf72b62e723d34c9711c6
Olesya Tenkova <olesya.tenkova@simbirsoft.com>
2019-07-02 12:40:42 +0400

    fix: rename items to children, fix syntax

--- packages/desktop/src/header-menu/HeaderMenu.tsx
+++ packages/desktop/src/header-menu/HeaderMenu.tsx
@@ -4,19 +4,17 @@ import {Flex, FlexItem} from '@qiwi/pijma-core'
 import {HeaderMenuItem, HeaderMenuItemProps} from './HeaderMenuItem'
 
 export interface HeaderMenuProps {
-  items: HeaderMenuItemProps[]
+  children: HeaderMenuItemProps[]
 }
 
-export const HeaderMenu: FC<HeaderMenuProps> = props => (
+export const HeaderMenu: FC<HeaderMenuProps> = (props) => (
   <Flex height={1} width={1} justify="flex-start">
-    {props.items.map((item, i) => (
+    {props.children.map((item, i) => (
       <FlexItem ml={i > 0 ? 6 : 0}>
         <HeaderMenuItem
-          {...item}
           key={i}
-        >
-          {item.title}
-        </HeaderMenuItem>
+          {...item}
+        />
       </FlexItem>
     ))}
   </Flex>
