c7cbbe07b3c496c8aa7b33afe4acf04d57c03d0f
irinaa133 <irina.soldatenko156@mail.ru>
2019-08-06 09:55:50 +0300

    feat: change props

--- packages/core/src/section/Section.tsx
+++ packages/core/src/section/Section.tsx
@@ -2,10 +2,10 @@ import React, {FC} from 'react'
 
 import {Card} from '../primitive'
 
-export const Section: FC = (props) => (
+export const Section: FC = ({children}) => (
   <Card
     r={10}
     bg="#f5f5f5"
-    children={props.children}
+    children={children}
   />
 )
