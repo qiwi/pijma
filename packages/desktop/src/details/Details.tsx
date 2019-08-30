2b79d87512de0939e70e861fff130b66b37fc682
irinaa133 <irina.soldatenko156@mail.ru>
2019-08-30 13:23:10 +0300

    fix: removed titleWidth

--- packages/desktop/src/details/Details.tsx
+++ packages/desktop/src/details/Details.tsx
@@ -1,60 +1,103 @@
 import React, {FC} from 'react'
-import {Flex, Box} from '@qiwi/pijma-core'
-import {Paragraph, Text} from '../typography'
+import {Box, Pos} from '@qiwi/pijma-core'
+import {Text} from '../typography'
 
 export interface DetailsProps {
   children: {title?: string, content: string}[]
   dots?: boolean
-  /** % */
-  titleWidth?: number
 }
 
-export const Details: FC<DetailsProps> = ({children, dots, titleWidth = 50}) => (
+export const Details: FC<DetailsProps> = ({children, dots}) => (
   !dots ? (
-    <Flex direction="column">
+    <Box display="table">
       {children.map((item, i) => (
-        <Flex mt={i !== 0 ? 3 : undefined} align="baseline" key={i}>
-          <Box mt="auto" width={titleWidth + '%'}>
-            <Text bold={false} size="m" color="support" children={item['title']}/>
-          </Box>
-          <Box pl={3} mt="auto" width={100 - titleWidth + '%'}>
-            <Paragraph size="m" children={item['content']}/>
-          </Box>
-        </Flex>
-      ))}
-    </Flex>
-  ) : (
-    <Flex direction="column">
-      {children.map((item, i) => (
-        <Flex mt={i !== 0 ? 3 : undefined} key={i}>
+        <Box css={{display: 'table-row'}} key={i}>
           <Box
-            overflow="hidden"
-            mt="auto"
-            width={titleWidth + '%'}
+            pt={i !== 0 ? 3 : undefined}
             css={{
-              position: 'relative',
-              ':after': {
-                content: '""',
-                position: 'absolute',
-                bottom: '7px',
-                borderBottom: 'dashed 1px #e6e6e6',
-                width: '100%',
-                marginLeft: '8px',
-                marginRight: '8px',
-              },
+              display: 'table-cell',
+              verticalAlign: 'bottom',
             }}
           >
-            <Text bold={false} size="m" color="support" children={item['title']}/>
+            <Text
+              bold={false}
+              size="m"
+              color="support"
+              children={item.title}
+            />
           </Box>
-          <Box mt="auto" pl={2} width={100 - titleWidth + '%'}>
-            <Paragraph size="m" children={item['content']}/>
+          <Box
+            pl={4}
+            pt={i !== 0 ? 3 : undefined}
+            css={{
+              display: 'table-cell',
+              verticalAlign: 'bottom',
+            }}
+          >
+            <Text
+              bold={false}
+              size="m"
+              children={item.content}
+            />
           </Box>
-        </Flex>
+        </Box>
       ))}
-    </Flex>
+    </Box>
+  ) : (
+    <Box display="table">
+      {children.map((item, i) => (
+        <Box css={{display: 'table-row'}} key={i}>
+          <Box
+            pt={i !== 0 ? 3 : undefined}
+            minWidth={50}
+            css={{
+              display: 'table-cell',
+              verticalAlign: 'bottom',
+            }}
+          >
+            {item.title ? (
+              <Pos
+                type="relative"
+                overflow="hidden"
+                css={{
+                  ':after': {
+                    content: '""',
+                    position: 'absolute',
+                    bottom: '7px',
+                    borderBottom: 'dashed 1px #e6e6e6',
+                    width: '100%',
+                    marginLeft: '8px',
+                  },
+                }}
+              >
+                <Text
+                  bold={false}
+                  size="m"
+                  color="support"
+                  children={item.title}
+                />
+              </Pos>
+            ) : (
+              null
+            )}
+          </Box>
+          <Box
+            pt={i !== 0 ? 3 : undefined}
+            css={{
+              display: 'table-cell',
+              verticalAlign: 'bottom',
+            }}
+            pl={2}
+            minWidth={50}
+          >
+            <Text
+              bold={false}
+              size="m"
+              children={item.content}
+            />
+          </Box>
+        </Box>
+      ))}
+    </Box>
   )
 )
-
-Details.defaultProps = {
-  titleWidth: 50,
-}
