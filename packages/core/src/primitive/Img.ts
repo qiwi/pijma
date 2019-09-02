196861664a9b0195bfeaa7eb7e8e405d0d9cada4
snoosmoomrik <toxa85@gmail.com>
2019-09-02 10:27:29 +0300

    feat: image

---
+++ packages/core/src/primitive/Img.ts
@@ -0,0 +1,34 @@
+import styled, {CSSObject} from '../styled'
+import {Value, cssValue} from '../primitive/Value'
+
+export interface ImgProps {
+  src?: string
+  width?: Value
+  minWidth?: Value
+  maxWidth?: Value
+  height?: Value
+  minHeight?: Value
+  maxHeight?: Value
+  srcSet?: string
+  sizes?: string
+  alt?: string
+  css?: CSSObject
+}
+
+export const ImgNonProps = [
+  'css', 'innerRef', 'ref', 'display',
+  'width', 'minWidth', 'maxWidth',
+  'height', 'minHeight', 'maxHeight',
+]
+
+export const Img = styled('img', {
+  shouldForwardProp: (prop) => !ImgNonProps.includes(prop),
+})<ImgProps>(({theme, ...props}) => ({
+  display: 'block',
+  width: cssValue(props.width, theme.scale),
+  minWidth: cssValue(props.minWidth, theme.scale),
+  maxWidth: cssValue(props.maxWidth, theme.scale),
+  height: cssValue(props.height, theme.scale),
+  minHeight: cssValue(props.minHeight, theme.scale),
+  maxHeight: cssValue(props.maxHeight, theme.scale),
+}), (props) => props.css)
