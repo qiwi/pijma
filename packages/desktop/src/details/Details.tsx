a9c9017093f2a031956ac099abd771d43b4716be
irinaa133 <irina.soldatenko156@mail.ru>
2019-09-04 12:30:51 +0300

    style: removed duplicate code

--- packages/desktop/src/details/Details.tsx
+++ packages/desktop/src/details/Details.tsx
@@ -27,117 +27,56 @@ const bottomDots: { [size in NonNullable<DetailsProps['size']>]: string } = {
 export const Details: FC<DetailsProps> = ({children, dots, titleWidth, contentWidth, size = 'm'}) => (
   <Box as="dl" width="100%" display="table">
     {children.map((item, i) => (
-      Array.isArray(item.content) ? (
-        item.content.map((value, index) => (
-          <Box css={{display: 'table-row'}} key={index}>
-            <Box
-              as="dt"
-              width={titleWidth ? titleWidth : undefined}
-              pt={index !== 0 ? 2 : i !== 0 ? 4 : undefined}
-              css={{
-                display: 'table-cell',
-                verticalAlign: 'bottom',
-              }}
-            >
-              {index === 0 ? (
-                dots ? (
-                  <Pos
-                    type="relative"
-                    overflow="hidden"
-                    css={{
-                      ':after': {
-                        content: '""',
-                        position: 'absolute',
-                        bottom: bottomDots[size],
-                        borderBottom: 'dashed 1px #e6e6e6',
-                        width: '100%',
-                        marginLeft: indentDots[size],
-                      },
-                    }}
-                  >
-                    <Text
-                      bold={false}
-                      size={size}
-                      color="support"
-                      children={item.title}
-                    />
-                  </Pos>
-                ) : (
-                  <Text
-                    color="support"
-                    bold={false}
-                    size={size}
-                    children={item.title}
-                  />
-                )
-              ) : (
-                null
-              )}
-            </Box>
-            <Box
-              as="dd"
-              width={contentWidth ? contentWidth : undefined}
-              pt={index !== 0 ? 2 : i !== 0 ? 4 : undefined}
-              css={{
-                display: 'table-cell',
-                verticalAlign: 'bottom',
-              }}
-              pl={indentDots[size]}
-            >
-              <Text
-                bold={false}
-                size={size}
-                children={value}
-              />
-            </Box>
-          </Box>
-        ))
-      ) : (
-        <Box css={{display: 'table-row'}} key={i}>
+      ([] as (string | ReactElement)[]).concat(item.content).map((value, index) => (
+        <Box css={{display: 'table-row'}} key={index}>
           <Box
             as="dt"
             width={titleWidth ? titleWidth : undefined}
-            pt={i !== 0 ? 4 : undefined}
+            pt={index !== 0 ? 2 : i !== 0 ? 4 : undefined}
             css={{
               display: 'table-cell',
               verticalAlign: 'bottom',
             }}
           >
-            {dots ? (
-              <Pos
-                type="relative"
-                overflow="hidden"
-                css={{
-                  ':after': {
-                    content: '""',
-                    position: 'absolute',
-                    bottom: bottomDots[size],
-                    borderBottom: 'dashed 1px #e6e6e6',
-                    width: '100%',
-                    marginLeft: indentDots[size],
-                  },
-                }}
-              >
+            {index === 0 ? (
+              dots ? (
+                <Pos
+                  type="relative"
+                  overflow="hidden"
+                  css={{
+                    ':after': {
+                      content: '""',
+                      position: 'absolute',
+                      bottom: bottomDots[size],
+                      borderBottom: 'dashed 1px #e6e6e6',
+                      width: '100%',
+                      marginLeft: indentDots[size],
+                    },
+                  }}
+                >
+                  <Text
+                    bold={false}
+                    size={size}
+                    color="support"
+                    children={item.title}
+                  />
+                </Pos>
+              ) : (
                 <Text
+                  color="support"
                   bold={false}
                   size={size}
-                  color="support"
                   children={item.title}
                 />
-              </Pos>
+              )
             ) : (
-              <Text
-                color="support"
-                bold={false}
-                size={size}
-                children={item.title}
-              />
+              null
             )}
           </Box>
           <Box
             as="dd"
             width={contentWidth ? contentWidth : undefined}
-            pt={i !== 0 ? 4 : undefined}
+            pt={index !== 0 ? 2 : i !== 0 ? 4 : undefined}
             css={{
               display: 'table-cell',
               verticalAlign: 'bottom',
@@ -147,10 +86,11 @@ export const Details: FC<DetailsProps> = ({children, dots, titleWidth, contentWi
             <Text
               bold={false}
               size={size}
-              children={item.content}
+              children={value}
             />
           </Box>
         </Box>
-      )))}
+      ))
+    ))}
   </Box>
 )
