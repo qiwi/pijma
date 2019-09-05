733ec0e4784fe5047c36961a70f4d0309dd1b0d3
Anton Kharitonov <toxa85@gmail.com>
2019-09-05 11:49:43 +0300

    remove unused setter

--- packages/core/src/phone-field/Phone.ts
+++ packages/core/src/phone-field/Phone.ts
@@ -2,11 +2,7 @@ import Country from './Country'
 
 export default class Phone {
 
-  constructor(phoneNumber: string, countries: Country[], countryFallback: Country = {
-    name: 'Россия',
-    code: 'RU',
-    mask: '+7dddddddddd',
-  }) {
+  constructor(phoneNumber: string, countries: Country[], countryFallback: Country) {
     this.phoneNumber = phoneNumber
     this.countries = countries
     this.countryFallback = countryFallback
@@ -25,12 +21,6 @@ export default class Phone {
       .sort((a, b) => b.mask.replace(/\D/g, '').length - a.mask.replace(/\D/g, '').length)
       .find((option) => clearPhone.indexOf(option.mask.replace(/\D/g, '')) === 0)
     return country || this.countryFallback
-  }
-
-  public set country(country: Country) {
-    const currentCountryMask = this.country.mask.replace(/\D/g, '')
-    const newCountryMask = country.mask.replace(/\D/g, '')
-    this.phoneNumber = `+${newCountryMask}${this.phoneNumber.replace(/\D/g, '').substr(currentCountryMask.length)}`
   }
 
 }
