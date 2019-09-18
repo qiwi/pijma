5454fe745e80b4a346be5d5da45783855ae3f01a
k.sidorov <k.sidorov@qiwi.com>
2019-09-18 12:13:34 +0300

    rf @role for desktop/breadcrumbs

--- packages/desktop/src/breadcrumbs/Breadcrumbs.tsx
+++ packages/desktop/src/breadcrumbs/Breadcrumbs.tsx
@@ -4,17 +4,18 @@ import {NavLink, NavLinkProps} from '../nav-link'
 import {Paragraph} from '../typography'
 
 export interface BreadcrumbsProps {
+  title?: string
   children: NavLinkProps[]
 }
 
-export const Breadcrumbs: FC<BreadcrumbsProps> = ({children}) => (
-  <Box role="menubar" aria-label="Breadcrumbs">
+export const Breadcrumbs: FC<BreadcrumbsProps> = ({title, children}) => (
+  <Box as="nav" aria-label={title}>
     <Paragraph size="s" color="support">
       {children.map((item, i) => (
         <Fragment key={i} >
           {i > 0 ? (
             <Fragment>
-              <Box as="span" mx={2} display="inline" role="section">
+              <Box as="span" mx={2} display="inline">
                 &#8226;
               </Box>
               &shy;
