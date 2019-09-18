46fae2c0e02466dd972ab85954ff3339b791642c
irinaa133 <irina.soldatenko156@mail.ru>
2019-09-18 14:01:56 +0300

    fix: FooterIconLinks

--- packages/mobile/src/footer-icon-links/FooterIconLinks.tsx
+++ packages/mobile/src/footer-icon-links/FooterIconLinks.tsx
@@ -4,60 +4,72 @@ import {Flex, FlexItem, Lnk, Card, Icon, IconProps, LinkControl} from '@qiwi/pij
 
 const CardLink = Card.withComponent(Lnk)
 
+interface FooterIconLinkProps {
+  href: string,
+  target?: string,
+  download?: string | boolean,
+  rel?: string,
+  title?: string,
+  icon: IconProps['name'],
+  onClick?: (href?: string, target?: string, download?: string | boolean, rel?: string) => void,
+  onFocus?: () => void,
+  onBlur?: () => void
+}
+
+const FooterIconLink: FC<FooterIconLinkProps> = (props) => (
+  <FlexItem shrink={0} align="center" justify="center">
+    <LinkControl
+      href={props.href}
+      target={props.target}
+      download={props.download}
+      rel={props.rel}
+      onClick={props.onClick}
+      onFocus={props.onFocus}
+      onBlur={props.onBlur}
+      children={(renderProps) => (
+        <CardLink
+          display="block"
+          width={10}
+          height={10}
+          r={40}
+          p={2}
+          s={renderProps.hover || renderProps.focus || renderProps.active ? '0 0 0 1px #000' : '0 0 0 1px #ccc'}
+          href={props.href}
+          rel={props.rel}
+          target={props.target}
+          title={props.title}
+          download={props.download}
+          onClick={renderProps.onClick}
+          onFocus={renderProps.onFocus}
+          onBlur={renderProps.onBlur}
+          onMouseEnter={renderProps.onMouseEnter}
+          onMouseLeave={renderProps.onMouseLeave}
+          onMouseUp={renderProps.onMouseUp}
+          onMouseDown={renderProps.onMouseDown}
+          children={
+            <Icon
+              name={props.icon}
+              color={renderProps.hover || renderProps.focus || renderProps.active ? '#000' : '#999'}
+            />
+          }
+        />
+      )}
+    />
+  </FlexItem>
+)
+
 export interface FooterIconLinksProps {
-  children: {
-    href: string,
-    target?: string,
-    download?: string | boolean,
-    rel?: string,
-    title?: string,
-    icon: IconProps['name'],
-    onClick?: (href?: string, target?: string, download?: string | boolean, rel?: string) => void,
-    onFocus?: () => void,
-    onBlur?: () => void
-  }[]
+  children: FooterIconLinkProps[]
 }
 
 export const FooterIconLinks: FC<FooterIconLinksProps> = ({children}) => (
   <Flex justify="space-between">
     {children.map((item, i) => (
-      <FlexItem key={i} width={10} height={10} shrink={0} align="center" justify="center">
-        <LinkControl
-          href={item.href}
-          target={item.target}
-          download={item.download}
-          rel={item.rel}
-          onClick={item.onClick}
-          onFocus={item.onFocus}
-          onBlur={item.onBlur}
-          children={(renderProps) => (
-            <CardLink
-              display="block"
-              r={40}
-              p={2}
-              s={renderProps.hover || renderProps.focus || renderProps.active ? '0 0 0 1px #000' : '0 0 0 1px #ccc'}
-              href={item.href}
-              rel={item.rel}
-              target={item.target}
-              title={item.title}
-              download={item.download}
-              onClick={renderProps.onClick}
-              onFocus={renderProps.onFocus}
-              onBlur={renderProps.onBlur}
-              onMouseEnter={renderProps.onMouseEnter}
-              onMouseLeave={renderProps.onMouseLeave}
-              onMouseUp={renderProps.onMouseUp}
-              onMouseDown={renderProps.onMouseDown}
-              children={
-                <Icon
-                  name={item.icon}
-                  color={renderProps.hover || renderProps.focus || renderProps.active ? '#000' : '#999'}
-                />
-              }
-            />
-          )}
-        />
-      </FlexItem>
+      <FooterIconLink
+        key={i}
+        href={item.href}
+        icon={item.icon}
+      />
     ))}
   </Flex>
 )
