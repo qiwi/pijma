58e4d7ccf8cb6d063213bac941a2a152874e1ad3
Maxim Kuznetsov <fixer40@bk.ru>
2019-09-26 16:54:04 +0300

    feat: textarea

---
+++ packages/core/src/text-area-field/TextAreaFieldControlProps.ts
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import RenderChild from '../RenderChild'
+
+export default interface TextAreaFieldControlProps {
+  value: string
+  onChange?: (value: string) => void
+  onFocus?: () => void
+  onBlur?: () => void
+  onKeyDown?: (event: React.KeyboardEvent) => boolean
+  onKeyUp?: (event: React.KeyboardEvent) => boolean
+  children: RenderChild<{
+    rows: number
+    focused: boolean
+    animate: boolean
+    ref: React.RefObject<HTMLTextAreaElement>
+    onChange: React.ChangeEventHandler
+    onFocus: React.FocusEventHandler
+    onBlur: React.FocusEventHandler
+    onKeyDown: React.KeyboardEventHandler
+    onKeyUp: React.KeyboardEventHandler
+  }>
+}
---
+++ packages/desktop/src/text-area-field/TextAreaField.tsx
@@ -0,0 +1,69 @@
+import React, {FC, KeyboardEvent, ReactNode} from 'react'
+
+import {TextAreaFieldControl, InputField, BasicTextArea} from '@qiwi/pijma-core'
+
+export interface TextAreaFieldProps {
+  value: string
+  tabIndex?: number
+  name?: string
+  title?: string
+  error?: ReactNode
+  action?: ReactNode
+  help?: ReactNode
+  autoFocus?: boolean
+  placeholder?: string
+  disabled?: boolean
+  maxLength?: number
+  minRows?: number
+  maxRows?: number
+  onChange?: (value: string) => void
+  onFocus?: () => void
+  onBlur?: () => void
+  onKeyDown?: (event: KeyboardEvent) => boolean
+  onKeyUp?: (event: KeyboardEvent) => boolean
+}
+
+export const TextAreaField: FC<TextAreaFieldProps> = (props) => (
+  <TextAreaFieldControl
+    onChange={props.onChange}
+    onFocus={props.onFocus}
+    onBlur={props.onBlur}
+    onKeyDown={props.onKeyDown}
+    onKeyUp={props.onKeyUp}
+    value={props.value}
+    children={(renderProps) => (
+      <InputField
+        title={props.title}
+        active={renderProps.focused || !!props.value || !!props.placeholder}
+        input={(
+          <BasicTextArea
+            value={props.value}
+            name={props.name}
+            autoFocus={props.autoFocus}
+            placeholder={props.placeholder}
+            disabled={props.disabled}
+            error={!!props.error}
+            focused={renderProps.focused}
+            maxLength={props.maxLength}
+            ref={renderProps.ref}
+            rows={props.maxRows && renderProps.rows > props.maxRows ? props.maxRows : props.minRows && renderProps.rows < props.minRows ? props.minRows : renderProps.rows}
+            overflow={props.maxRows && renderProps.rows > props.maxRows ? 'auto' : 'hidden'}
+            transition={renderProps.animate ? 'all 100ms cubic-bezier(0.4, 0.0, 0.2, 1)' : undefined}
+            onChange={renderProps.onChange}
+            onFocus={renderProps.onFocus}
+            onBlur={renderProps.onBlur}
+            onKeyDown={renderProps.onKeyDown}
+            onKeyUp={renderProps.onKeyUp}
+          />
+        )}
+        error={props.error}
+        help={props.help}
+        action={props.action}
+      />
+    )}
+  />
+)
+
+TextAreaField.defaultProps = {
+  tabIndex: 0,
+}
