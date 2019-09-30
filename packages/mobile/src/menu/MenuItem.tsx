e9f785a8cbf78661b2d93ad694adf78220c08132
irinaa133 <irina.soldatenko156@mail.ru>
2019-09-30 13:55:51 +0300

    feat: menu-link

--- packages/mobile/src/menu/MenuItem.tsx
+++ packages/mobile/src/menu/MenuItem.tsx
@@ -1,88 +1,59 @@
 import React, {FC, ReactNode} from 'react'
 
-import {Flex, FlexItem, Lnk, Card, LinkControl, LinkControlProps, Icon, Box} from '@qiwi/pijma-core'
+import {Flex, FlexItem, Icon, Box} from '@qiwi/pijma-core'
+
+import {MenuLink, MenuLinkProps} from './MenuLink'
 
 import {Paragraph} from '../typography'
 
-export interface MenuItemProps {
+export interface MenuItemProps extends MenuLinkProps {
   text: string
   notes?: string
   icon?: ReactNode
   submenu?: boolean
-  tabIndex?: number
-  href?: string
-  target?: string
-  download?: string | boolean
-  rel?: string
-  title?: string
-  onClick?: LinkControlProps['onClick']
-  onFocus?: LinkControlProps['onFocus']
-  onBlur?: LinkControlProps['onBlur']
 }
 
-const CardLink = Card.withComponent(Lnk)
-
 export const MenuItem: FC<MenuItemProps> = (props) => (
-  <LinkControl
+  <MenuLink
+    tabIndex={props.tabIndex}
     href={props.href}
     target={props.target}
     download={props.download}
     rel={props.rel}
+    title={props.title}
     onClick={props.onClick}
     onFocus={props.onFocus}
     onBlur={props.onBlur}
-    children={(renderProps) => (
-      <CardLink
-        bg={renderProps.hover || renderProps.focus || renderProps.active ? '#f5f5f5' : undefined}
-        cursor="pointer"
-        display="block"
-        px={4}
-        transition="all 300ms cubic-bezier(0.4, 0.0, 0.2, 1)"
-        tabIndex={props.tabIndex}
-        href={props.href}
-        title={props.title}
-        target={props.target}
-        download={props.download}
-        rel={props.rel}
-        onClick={renderProps.onClick}
-        onFocus={renderProps.onFocus}
-        onBlur={renderProps.onBlur}
-        onMouseEnter={renderProps.onMouseEnter}
-        onMouseLeave={renderProps.onMouseLeave}
-        onMouseUp={renderProps.onMouseUp}
-        onMouseDown={renderProps.onMouseDown}
-      >
-        <Flex py={2} minHeight={14}>
-          {props.icon ? (
-            <FlexItem align={props.notes ? undefined : 'center'} shrink={0} mr={4}>
-              {props.icon}
-            </FlexItem>
-          ) : (
-            null
-          )}
-          <FlexItem align="center" grow={1}>
-            <Flex justify="center" direction="column">
-              <Paragraph clamp={props.icon && !props.notes ? 2 : undefined} bold>{props.text}</Paragraph>
-              {props.notes ? (
-                <Box mt={1}>
-                  <Paragraph size="s" color="support">
-                    {props.notes}
-                  </Paragraph>
-                </Box>
-              ) : (
-                null
-              )}
-            </Flex>
-          </FlexItem>
-          {props.submenu ? (
-            <FlexItem align="center" shrink={0} width={6} height={6} ml={3}>
-              <Icon name="angle-right"/>
-            </FlexItem>
+  >
+    <Flex py={2} minHeight={14}>
+      {props.icon ? (
+        <FlexItem align={props.notes ? undefined : 'center'} shrink={0} mr={4}>
+          {props.icon}
+        </FlexItem>
+      ) : (
+        null
+      )}
+      <FlexItem align="center" grow={1}>
+        <Flex justify="center" direction="column">
+          <Paragraph clamp={props.icon && !props.notes ? 2 : undefined} bold>{props.text}</Paragraph>
+          {props.notes ? (
+            <Box mt={1}>
+              <Paragraph size="s" color="support">
+                {props.notes}
+              </Paragraph>
+            </Box>
           ) : (
             null
           )}
         </Flex>
-      </CardLink>
-    )}
-  />
+      </FlexItem>
+      {props.submenu ? (
+        <FlexItem align="center" shrink={0} width={6} height={6} ml={3}>
+          <Icon name="angle-right"/>
+        </FlexItem>
+      ) : (
+        null
+      )}
+    </Flex>
+  </MenuLink>
 )
---
+++ packages/mobile/src/menu/MenuLink.tsx
@@ -0,0 +1,52 @@
+import React, {FC} from 'react'
+
+import {Lnk, Card, LinkControl, LinkControlProps} from '@qiwi/pijma-core'
+
+export interface MenuLinkProps {
+  tabIndex?: number
+  href?: string
+  target?: string
+  download?: string | boolean
+  rel?: string
+  title?: string
+  onClick?: LinkControlProps['onClick']
+  onFocus?: LinkControlProps['onFocus']
+  onBlur?: LinkControlProps['onBlur']
+}
+
+const CardLink = Card.withComponent(Lnk)
+
+export const MenuLink: FC<MenuLinkProps> = (props) => (
+  <LinkControl
+    href={props.href}
+    target={props.target}
+    download={props.download}
+    rel={props.rel}
+    onClick={props.onClick}
+    onFocus={props.onFocus}
+    onBlur={props.onBlur}
+    children={(renderProps) => (
+      <CardLink
+        bg={renderProps.hover || renderProps.focus || renderProps.active ? '#f5f5f5' : undefined}
+        cursor="pointer"
+        display="block"
+        px={4}
+        transition="all 300ms cubic-bezier(0.4, 0.0, 0.2, 1)"
+        tabIndex={props.tabIndex}
+        href={props.href}
+        title={props.title}
+        target={props.target}
+        download={props.download}
+        rel={props.rel}
+        onClick={renderProps.onClick}
+        onFocus={renderProps.onFocus}
+        onBlur={renderProps.onBlur}
+        onMouseEnter={renderProps.onMouseEnter}
+        onMouseLeave={renderProps.onMouseLeave}
+        onMouseUp={renderProps.onMouseUp}
+        onMouseDown={renderProps.onMouseDown}
+        children={props.children}
+      />
+    )}
+  />
+)
