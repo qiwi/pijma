018cea53dfe21d322aa03dcc9014f3eeb72fd4ee
AlekseyShatohin <56636950+AlekseyShatohin@users.noreply.github.com>
2019-10-23 11:37:21 +0300

    fix: PR fixes

--- packages/desktop/src/progress-bar/ProgressBar.tsx
+++ packages/desktop/src/progress-bar/ProgressBar.tsx
@@ -7,7 +7,7 @@ export interface ProgressBarProps {
 }
 
 export const ProgressBar: FC<ProgressBarProps> = ({value, disabled = false}) => (
-  <Card bg="#f5f5f5" height={10} r={24} p={2}>
+  <Card bg="#f5f5f5" height={10} r={20} p={2}>
     <Card
       bg={disabled ? '#ccc' : 'linear-gradient(268deg, #ff8c00 99%, #ff7700)'}
       height={6}
