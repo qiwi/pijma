1e3785743d41d945c11131ed3ca662b4f307db7b
Dima Livshits <dimonka83@gmail.com>
2019-10-28 11:18:03 +0300

    fix: no calculations

--- packages/desktop/src/typography/Paragraph.tsx
+++ packages/desktop/src/typography/Paragraph.tsx
@@ -80,7 +80,7 @@ export const Paragraph: FunctionComponent<ParagraphProps> = ({
 }) => (
   stub ? (
     <Fragment>
-      {Array(clamp === undefined ? 3 : clamp).fill(1).map((v, i) => i % 3 === 0 ? v - 0.2 : i % 3 === 1 ? v - 0.1 : v - 0.3).map((width: number, id: number) => (
+      {Array(clamp === undefined ? 3 : clamp).fill(0).map((_, i) => i % 3 === 0 ? 0.8 : i % 3 === 1 ? 0.9 : 0.7).map((width, id) => (
         <Box
           key={id}
           width={width}
