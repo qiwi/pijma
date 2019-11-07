ec9448fdd073f3bbca615b546a2493a9a280ef2d
AlekseyShatohin <56636950+AlekseyShatohin@users.noreply.github.com>
2019-11-07 17:09:20 +0300

    feat: stub blockAccordion

--- packages/desktop/src/block-accordion/BlockAccordion.tsx
+++ packages/desktop/src/block-accordion/BlockAccordion.tsx
@@ -1,6 +1,6 @@
 import React, {FunctionComponent, ReactNode} from 'react'
 
-import {AccordionControl, Box, Card, Flex, FlexItem, Icon} from '@qiwi/pijma-core'
+import {AccordionControl, Box, Card, Flex, FlexItem, Icon, Stub} from '@qiwi/pijma-core'
 
 import {Paragraph} from '../typography'
 
@@ -9,6 +9,7 @@ export interface BlockAccordionProps<I> {
   opened: number[]
   indent?: 's' | 'm' | 'l'
   tabIndex?: number
+  stub?: boolean
   onChange: (opened: number[]) => void
 }
 
@@ -25,72 +26,119 @@ const BlockAccordionIndent: {
   l: 17,
 }
 
-export const BlockAccordion: FunctionComponent<BlockAccordionProps<BlockAccordionItemModel>> = ({items, indent = 'm', tabIndex = 0, opened, onChange}) => (
-  <AccordionControl<BlockAccordionItemModel>
-    items={items}
-    opened={opened}
-    onChange={onChange}
-    children={renderProps => (
-      <Box py={3}>
-        {renderProps.items.map((item, index) => (
-          <Card
-            key={index}
-            s={
-              item.hovered || item.focused
-                ? '0 0 16px 0 rgba(0, 0, 0, 0.12)'
-                : index > 0 &&
-                !(renderProps.items[index - 1].hovered ||
-                  renderProps.items[index - 1].focused)
-                ? '0 -1px 0 #e6e6e6'
-                : undefined
-            }
-            transition="box-shadow 100ms cubic-bezier(0.4, 0.0, 0.2, 1)"
-            onMouseEnter={item.onMouseEnter}
-            onMouseLeave={item.onMouseLeave}
+export const BlockAccordion: FunctionComponent<BlockAccordionProps<BlockAccordionItemModel>> = ({stub = false, items, indent = 'm', tabIndex = 0, opened, onChange}) => (
+  stub ? (
+    <Box py={3}>
+      {[33, 33, 33, 33, 33].map((width, index) => (
+        <Card
+          key={index}
+          s={index > 0 ? '0 -1px 0 #e6e6e6' : undefined}
+        >
+          <Flex
+            wrap="nowrap"
+            justify="space-between"
+            align="center"
+            px={BlockAccordionIndent[indent]}
           >
-            <Flex
-              tabIndex={tabIndex}
-              wrap="nowrap"
-              justify="space-between"
-              align="start"
-              cursor="pointer"
-              px={BlockAccordionIndent[indent]}
-              pt={4}
-              pb={item.opened ? 2 : 4}
-              onClick={item.onClick}
-              onFocus={item.onFocus}
-              onBlur={item.onBlur}
-              onKeyDown={renderProps.onKeyDown}
+            <Paragraph size="m">
+              <Stub
+                height={2}
+                width={opened.includes(index) ? 56 : width}
+                top={6}
+                bottom={6}
+              />
+            </Paragraph>
+            <FlexItem
+              shrink={0}
+              width={6}
+              height={6}
+              ml={3}
+              transform={`rotate(${opened.includes(index) ? 180 : 0}deg)`}
             >
-              <Paragraph bold size="m">
-                {item.title}
-              </Paragraph>
-              <FlexItem
-                shrink={0}
-                width={6}
-                height={6}
-                ml={3}
-                transform={`rotate(${item.opened ? 180 : 0}deg)`}
-                transition="transform 0.3s ease-in-out"
+              <Icon name="angle-small-down"/>
+            </FlexItem>
+          </Flex>
+          <Box px={BlockAccordionIndent[indent]} display={opened.includes(index) ? 'block' : 'none'}>
+            <Paragraph size="m">
+              <Stub
+                height={2}
+                width={width}
+                bottom={6}
+              />
+            </Paragraph>
+          </Box>
+        </Card>
+      ))}
+    </Box>
+  ) : (
+    <AccordionControl<BlockAccordionItemModel>
+      items={items}
+      opened={opened}
+      onChange={onChange}
+      children={renderProps => (
+        <Box py={3}>
+          {renderProps.items.map((item, index) => (
+            <Card
+              key={index}
+              s={
+                item.hovered || item.focused
+                  ? '0 0 16px 0 rgba(0, 0, 0, 0.12)'
+                  : index > 0 &&
+                  !(renderProps.items[index - 1].hovered ||
+                    renderProps.items[index - 1].focused)
+                  ? '0 -1px 0 #e6e6e6'
+                  : undefined
+              }
+              transition="box-shadow 100ms cubic-bezier(0.4, 0.0, 0.2, 1)"
+              onMouseEnter={item.onMouseEnter}
+              onMouseLeave={item.onMouseLeave}
+            >
+              <Flex
+                tabIndex={tabIndex}
+                wrap="nowrap"
+                justify="space-between"
+                align="start"
+                cursor="pointer"
+                px={BlockAccordionIndent[indent]}
+                pt={4}
+                pb={item.opened ? 2 : 4}
+                onClick={item.onClick}
+                onFocus={item.onFocus}
+                onBlur={item.onBlur}
+                onKeyDown={renderProps.onKeyDown}
               >
-                <Icon name="angle-small-down"/>
-              </FlexItem>
-            </Flex>
-            <Box px={BlockAccordionIndent[indent]} pb={4} display={item.opened ? 'block' : 'none'}>
-              {typeof item.content === 'string' ? (
-                <Paragraph size="m">{item.content}</Paragraph>
-              ) : (
-                item.content
-              )}
-            </Box>
-          </Card>
-        ))}
-      </Box>
-    )}
-  />
+                <Paragraph bold size="m">
+                  {item.title}
+                </Paragraph>
+                <FlexItem
+                  shrink={0}
+                  width={6}
+                  height={6}
+                  ml={3}
+                  transform={`rotate(${item.opened ? 180 : 0}deg)`}
+                  transition="transform 0.3s ease-in-out"
+                >
+                  <Icon name="angle-small-down"/>
+                </FlexItem>
+              </Flex>
+              <Box px={BlockAccordionIndent[indent]} pb={4} display={item.opened ? 'block' : 'none'}>
+                {typeof item.content === 'string' ? (
+                  <Paragraph size="m">{item.content}</Paragraph>
+                ) : (
+                  item.content
+                )}
+              </Box>
+            </Card>
+          ))}
+        </Box>
+      )}
+    />
+  )
+
 )
 
 BlockAccordion.defaultProps = {
   indent: 'm',
   tabIndex: 0,
+  stub: false,
 }
--- packages/mobile/src/block-accordion/BlockAccordion.tsx
+++ packages/mobile/src/block-accordion/BlockAccordion.tsx
@@ -1,6 +1,6 @@
 import React, {FunctionComponent, ReactNode} from 'react'
 
-import {AccordionControl, Box, Card, Flex, FlexItem, Icon} from '@qiwi/pijma-core'
+import {AccordionControl, Box, Card, Flex, FlexItem, Icon, Stub} from '@qiwi/pijma-core'
 
 import {Paragraph} from '../typography'
 
@@ -8,6 +8,7 @@ export interface BlockAccordionProps<I> {
   items: I[]
   opened: number[]
   tabIndex?: number
+  stub?: boolean
   onChange: (opened: number[]) => void
 }
 
@@ -16,67 +17,114 @@ export interface BlockAccordionItemModel {
   content: ReactNode
 }
 
-export const BlockAccordion: FunctionComponent<BlockAccordionProps<BlockAccordionItemModel>> = ({items, tabIndex = 0, opened, onChange}) => (
-  <AccordionControl<BlockAccordionItemModel>
-    items={items}
-    opened={opened}
-    onChange={onChange}
-    children={renderProps => (
-      <Box py={2}>
-        {renderProps.items.map((item, index) => (
-          <Card
-            key={index}
-            s={
-              index > 0
-                ? '0 -1px 0 #e6e6e6'
-                : undefined
-            }
-            transition="box-shadow 100ms cubic-bezier(0.4, 0.0, 0.2, 1)"
-            onMouseEnter={item.onMouseEnter}
-            onMouseLeave={item.onMouseLeave}
+export const BlockAccordion: FunctionComponent<BlockAccordionProps<BlockAccordionItemModel>> = ({stub = false, items, tabIndex = 0, opened, onChange}) => (
+  stub ? (
+    <Box py={2}>
+      {[33, 33, 33, 33, 33].map((width, index) => (
+        <Card
+          key={index}
+          s={index > 0 ? '0 -1px 0 #e6e6e6' : undefined}
+        >
+          <Flex
+            wrap="nowrap"
+            justify="space-between"
+            align="center"
+            px={4}
           >
-            <Flex
-              tabIndex={tabIndex}
-              wrap="nowrap"
-              justify="space-between"
-              align="start"
-              cursor="pointer"
-              px={4}
-              pt={4}
-              pb={item.opened ? 1 : 4}
-              onClick={item.onClick}
-              onFocus={item.onFocus}
-              onBlur={item.onBlur}
-              onKeyDown={renderProps.onKeyDown}
+            <Paragraph size="m">
+              <Stub
+                height={2}
+                width={opened.includes(index) ? 56 : width}
+                top={6}
+                bottom={6}
+              />
+            </Paragraph>
+            <FlexItem
+              shrink={0}
+              width={6}
+              height={6}
+              ml={3}
+              transform={`rotate(${opened.includes(index) ? 180 : 0}deg)`}
             >
-              <Paragraph bold size="s">
-                {item.title}
-              </Paragraph>
-              <FlexItem
-                shrink={0}
-                width={6}
-                height={6}
-                ml={3}
-                transform={`rotate(${item.opened ? 180 : 0}deg)`}
-                transition="transform 0.3s ease-in-out"
+              <Icon name="angle-small-down"/>
+            </FlexItem>
+          </Flex>
+          <Box px={4} display={opened.includes(index) ? 'block' : 'none'}>
+            <Paragraph size="m">
+              <Stub
+                height={2}
+                width={width}
+                bottom={6}
+              />
+            </Paragraph>
+          </Box>
+        </Card>
+      ))}
+    </Box>
+  ) : (
+    <AccordionControl<BlockAccordionItemModel>
+      items={items}
+      opened={opened}
+      onChange={onChange}
+      children={renderProps => (
+        <Box py={2}>
+          {renderProps.items.map((item, index) => (
+            <Card
+              key={index}
+              s={
+                index > 0
+                  ? '0 -1px 0 #e6e6e6'
+                  : undefined
+              }
+              transition="box-shadow 100ms cubic-bezier(0.4, 0.0, 0.2, 1)"
+              onMouseEnter={item.onMouseEnter}
+              onMouseLeave={item.onMouseLeave}
+            >
+              <Flex
+                tabIndex={tabIndex}
+                wrap="nowrap"
+                justify="space-between"
+                align="start"
+                cursor="pointer"
+                px={4}
+                pt={4}
+                pb={item.opened ? 1 : 4}
+                onClick={item.onClick}
+                onFocus={item.onFocus}
+                onBlur={item.onBlur}
+                onKeyDown={renderProps.onKeyDown}
               >
-                <Icon name="angle-small-down"/>
-              </FlexItem>
-            </Flex>
-            <Box px={4} pb={4} display={item.opened ? 'block' : 'none'}>
-              {typeof item.content === 'string' ? (
-                <Paragraph size="s">{item.content}</Paragraph>
-              ) : (
-                item.content
-              )}
-            </Box>
-          </Card>
-        ))}
-      </Box>
-    )}
-  />
+                <Paragraph bold size="s">
+                  {item.title}
+                </Paragraph>
+                <FlexItem
+                  shrink={0}
+                  width={6}
+                  height={6}
+                  ml={3}
+                  transform={`rotate(${item.opened ? 180 : 0}deg)`}
+                  transition="transform 0.3s ease-in-out"
+                >
+                  <Icon name="angle-small-down"/>
+                </FlexItem>
+              </Flex>
+              <Box px={4} pb={4} display={item.opened ? 'block' : 'none'}>
+                {typeof item.content === 'string' ? (
+                  <Paragraph size="s">{item.content}</Paragraph>
+                ) : (
+                  item.content
+                )}
+              </Box>
+            </Card>
+          ))}
+        </Box>
+      )}
+    />
+  )
+
 )
 
 BlockAccordion.defaultProps = {
   tabIndex: 0,
+  stub: false,
 }
