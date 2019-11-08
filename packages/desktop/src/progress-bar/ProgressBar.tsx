2c99a1d4c7bf94b9eacf079625fc6817fb480787
Aleksey Shatokhin <56636950+AlekseyShatohin@users.noreply.github.com>
2019-11-08 16:42:23 +0300

    fix: progress-bar stub

--- packages/desktop/src/progress-bar/ProgressBar.tsx
+++ packages/desktop/src/progress-bar/ProgressBar.tsx
@@ -1,20 +1,30 @@
 import React, {FC} from 'react'
-import {Card} from '@qiwi/pijma-core'
+import {Card, Stub} from '@qiwi/pijma-core'
 
 export interface ProgressBarProps {
   value: number
   disabled?: boolean
+  stub?: boolean
 }
 
-export const ProgressBar: FC<ProgressBarProps> = ({value, disabled = false}) => (
-  <Card bg="#f5f5f5" height={10} r={20} p={2}>
-    <Card
-      bg={disabled ? '#ccc' : 'linear-gradient(268deg, #ff8c00 99%, #ff7700)'}
-      height={6}
-      width={value}
-      transition="width 300ms cubic-bezier(0.4, 0.0, 0.2, 1)"
-      minWidth={6}
-      r={12}
-    />
-  </Card>
+export const ProgressBar: FC<ProgressBarProps> = ({
+  value,
+  disabled = false,
+  stub = false,
+}) => (
+  stub ? (
+    <Stub height={10} width={1} r={20}/>
+  ) : (
+    <Card bg="#f5f5f5" height={10} r={20} p={2}>
+      <Card
+        bg={disabled ? '#ccc' : 'linear-gradient(268deg, #ff8c00 99%, #ff7700)'}
+        height={6}
+        width={value}
+        transition="width 300ms cubic-bezier(0.4, 0.0, 0.2, 1)"
+        minWidth={6}
+        r={12}
+      />
+    </Card>
+  )
+
 )
