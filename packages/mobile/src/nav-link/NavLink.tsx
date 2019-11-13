fa9ac1c155c5dabd68b59648b6c32f0eff62412f
AlekseyShatohin <56636950+AlekseyShatohin@users.noreply.github.com>
2019-11-13 12:30:56 +0300

    feat: stub nav-link

--- packages/mobile/src/nav-link/NavLink.tsx
+++ packages/mobile/src/nav-link/NavLink.tsx
@@ -1,5 +1,6 @@
 import React, {FC} from 'react'
-import {LinkControl, Lnk, Typo} from '@qiwi/pijma-core'
+import {Box, LinkControl, Lnk, Typo} from '@qiwi/pijma-core'
+import {Text} from '@qiwi/pijma-desktop/typography'
 
 export interface NavLinkProps {
   onClick?: (href?: string, target?: string, download?: string | boolean, rel?: string) => void
@@ -11,43 +12,50 @@ export interface NavLinkProps {
   download?: string | boolean
   rel?: string
   title?: string
+  stub?: boolean
 }
 
 const TypoLink = Typo.withComponent(Lnk)
 
 export const NavLink: FC<NavLinkProps> = (props) => (
-  <LinkControl
-    onClick={props.onClick}
-    onFocus={props.onFocus}
-    onBlur={props.onBlur}
-    href={props.href}
-    target={props.target}
-    download={props.download}
-    rel={props.rel}
-    children={(renderProps) => (
-      <TypoLink
-        tabIndex={props.tabIndex}
-        href={props.href}
-        onClick={renderProps.onClick}
-        onFocus={renderProps.onFocus}
-        onBlur={renderProps.onBlur}
-        onMouseEnter={renderProps.onMouseEnter}
-        onMouseLeave={renderProps.onMouseLeave}
-        onMouseUp={renderProps.onMouseUp}
-        onMouseDown={renderProps.onMouseDown}
-        color={renderProps.hover || renderProps.focus ? '#000' : '#666'}
-        transition="all 100ms cubic-bezier(0.4, 0.0, 0.2, 1)"
-        cursor="pointer"
-        decoration="none"
-        target={props.target}
-        download={props.download}
-        rel={props.rel}
-        title={props.title}
-        size={3.5}
-        height={5}
-        weight={300}
-        children={props.children}
-      />
-    )}
-  />
+  props.stub ? (
+    <Box width={12}>
+      <Text display="block" size="s" stub/>
+    </Box>
+  ) : (
+    <LinkControl
+      onClick={props.onClick}
+      onFocus={props.onFocus}
+      onBlur={props.onBlur}
+      href={props.href}
+      target={props.target}
+      download={props.download}
+      rel={props.rel}
+      children={(renderProps) => (
+        <TypoLink
+          tabIndex={props.tabIndex}
+          href={props.href}
+          onClick={renderProps.onClick}
+          onFocus={renderProps.onFocus}
+          onBlur={renderProps.onBlur}
+          onMouseEnter={renderProps.onMouseEnter}
+          onMouseLeave={renderProps.onMouseLeave}
+          onMouseUp={renderProps.onMouseUp}
+          onMouseDown={renderProps.onMouseDown}
+          color={renderProps.hover || renderProps.focus ? '#000' : '#666'}
+          transition="all 100ms cubic-bezier(0.4, 0.0, 0.2, 1)"
+          cursor="pointer"
+          decoration="none"
+          target={props.target}
+          download={props.download}
+          rel={props.rel}
+          title={props.title}
+          size={3.5}
+          height={5}
+          weight={300}
+          children={props.children}
+        />
+      )}
+    />
+    )
 )
