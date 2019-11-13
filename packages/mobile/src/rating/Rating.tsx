6203cd8cb005e141700fa0031c692ea3c5b86457
Aleksey Shatokhin <56636950+AlekseyShatohin@users.noreply.github.com>
2019-11-13 15:00:46 +0300

    feat: rating

---
+++ packages/mobile/src/rating/Rating.tsx
@@ -0,0 +1,83 @@
+import React, {FC} from 'react'
+import {Flex, FlexItem, Icon, RatingControl, Stub} from '@qiwi/pijma-core'
+
+export interface RatingProps {
+  value?: number
+  size?: 's' | 'm'
+  disabled?: boolean
+  count?: number
+  stub?: boolean
+  onChange: (value: number) => void
+}
+
+const RatingSize: Record<NonNullable<RatingProps['size']>, number> = {
+  s: 6,
+  m: 12,
+}
+
+const RatingIndent: Record<NonNullable<RatingProps['size']>, number> = {
+  s: 2,
+  m: 5,
+}
+
+export const Rating: FC<RatingProps> = ({
+  value = 0,
+  size = 'm',
+  disabled = false,
+  count = 5,
+  stub = false,
+  onChange,
+}) => (
+  stub ? (
+    <Flex>
+      {Array(count).fill(1).map((_, index) => (
+        <FlexItem
+          key={index}
+          pl={index === 0 ? 0 : RatingIndent[size]}
+        >
+          <Stub
+            height={RatingSize[size]}
+            width={RatingSize[size]}
+            r="50%"
+          />
+        </FlexItem>
+      ))}
+    </Flex>
+  ) : (
+    <RatingControl
+      value={value}
+      count={count}
+      disabled={disabled}
+      onChange={onChange}
+      children={renderProps => (
+        <Flex>
+          {renderProps.items.map((item, index) => (
+            <FlexItem
+              key={index}
+              pl={index === 0 ? 0 : RatingIndent[size] / 2}
+              pr={index === count - 1 ? 0 : RatingIndent[size] / 2}
+              onClick={item.onClick}
+              onMouseMove={item.onMouseEnter}
+              onMouseOut={item.onMouseLeave}
+            >
+              <Icon
+                name="star-solid"
+                size={RatingSize[size]}
+                color={item.active ? '#ff8c00' : '#cccccc'}
+              />
+            </FlexItem>
+          ))}
+        </Flex>
+      )}
+    />
+  )
+
+)
+
+Rating.defaultProps = {
+  value: 0,
+  size: 'm',
+  disabled: false,
+  count: 5,
+  stub: false,
+}
