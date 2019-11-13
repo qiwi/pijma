c8eda60761daff3d0782b362beda46dee6d84eb5
AlekseyShatohin <56636950+AlekseyShatohin@users.noreply.github.com>
2019-11-13 12:08:34 +0300

    feat: stub text-area-field

--- packages/mobile/src/text-area-field/TextAreaField.tsx
+++ packages/mobile/src/text-area-field/TextAreaField.tsx
@@ -1,6 +1,6 @@
 import React, {FC, KeyboardEvent, ReactNode} from 'react'
 
-import {TextAreaFieldControl, InputField, BasicTextArea} from '@qiwi/pijma-core'
+import {TextAreaFieldControl, InputField, BasicTextArea, Stub, Box, Card, Flex, FlexItem} from '@qiwi/pijma-core'
 
 export interface TextAreaFieldProps {
   value: string
@@ -16,6 +16,7 @@ export interface TextAreaFieldProps {
   maxLength?: number
   minRows?: number
   maxRows?: number
+  stub?: boolean
   onChange?: (value: string) => void
   onFocus?: () => void
   onBlur?: () => void
@@ -24,44 +25,66 @@ export interface TextAreaFieldProps {
 }
 
 export const TextAreaField: FC<TextAreaFieldProps> = (props) => (
-  <TextAreaFieldControl
-    onChange={props.onChange}
-    onFocus={props.onFocus}
-    onBlur={props.onBlur}
-    onKeyDown={props.onKeyDown}
-    onKeyUp={props.onKeyUp}
-    value={props.value}
-    children={(renderProps) => (
-      <InputField
-        title={props.title}
-        active={renderProps.focused || !!props.value || !!props.placeholder}
-        input={(
-          <BasicTextArea
-            value={props.value}
-            name={props.name}
-            autoFocus={props.autoFocus}
-            placeholder={props.placeholder}
-            disabled={props.disabled}
-            error={!!props.error}
-            focused={renderProps.focused}
-            maxLength={props.maxLength}
-            ref={renderProps.ref}
-            rows={props.maxRows && renderProps.rows > props.maxRows ? props.maxRows : props.minRows && renderProps.rows < props.minRows ? props.minRows : renderProps.rows}
-            overflow={props.maxRows && renderProps.rows > props.maxRows ? 'auto' : 'hidden'}
-            transition={renderProps.animate ? 'all 100ms cubic-bezier(0.4, 0.0, 0.2, 1)' : undefined}
-            onChange={renderProps.onChange}
-            onFocus={renderProps.onFocus}
-            onBlur={renderProps.onBlur}
-            onKeyDown={renderProps.onKeyDown}
-            onKeyUp={renderProps.onKeyUp}
-          />
-        )}
-        error={props.error}
-        help={props.help}
-        action={props.action}
-      />
-    )}
-  />
+  props.stub ? (
+    <Box>
+      {props.title ? (
+        <Stub width={15} height={2} top={1} bottom={1}/>
+      ) : (
+        <Box height={4}/>
+      )}
+      <Card bb="1px solid rgba(0, 0, 0, 0.2)" height={7}>
+        <Flex align="center" justify="space-between" height={1}>
+          <FlexItem>
+            <Stub width={38} height={3} top={1} bottom={1}/>
+          </FlexItem>
+        </Flex>
+      </Card>
+      {props.help || props.error ? (
+        <Stub width={15} height={2} top={2} bottom={1}/>
+      ) : (
+        <Box height={5}/>
+      )}
+    </Box>
+  ) : (
+    <TextAreaFieldControl
+      onChange={props.onChange}
+      onFocus={props.onFocus}
+      onBlur={props.onBlur}
+      onKeyDown={props.onKeyDown}
+      onKeyUp={props.onKeyUp}
+      value={props.value}
+      children={(renderProps) => (
+        <InputField
+          title={props.title}
+          active={renderProps.focused || !!props.value || !!props.placeholder}
+          input={(
+            <BasicTextArea
+              value={props.value}
+              name={props.name}
+              autoFocus={props.autoFocus}
+              placeholder={props.placeholder}
+              disabled={props.disabled}
+              error={!!props.error}
+              focused={renderProps.focused}
+              maxLength={props.maxLength}
+              ref={renderProps.ref}
+              rows={props.maxRows && renderProps.rows > props.maxRows ? props.maxRows : props.minRows && renderProps.rows < props.minRows ? props.minRows : renderProps.rows}
+              overflow={props.maxRows && renderProps.rows > props.maxRows ? 'auto' : 'hidden'}
+              transition={renderProps.animate ? 'all 100ms cubic-bezier(0.4, 0.0, 0.2, 1)' : undefined}
+              onChange={renderProps.onChange}
+              onFocus={renderProps.onFocus}
+              onBlur={renderProps.onBlur}
+              onKeyDown={renderProps.onKeyDown}
+              onKeyUp={renderProps.onKeyUp}
+            />
+          )}
+          error={props.error}
+          help={props.help}
+          action={props.action}
+        />
+      )}
+    />
+  )
 )
 
 TextAreaField.defaultProps = {
