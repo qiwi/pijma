f538861906197e851911a9f6c04159f7a3a3e7d5
Aleksey Shatokhin <56636950+AlekseyShatohin@users.noreply.github.com>
2019-11-15 12:30:43 +0300

    feat: stub link

--- packages/desktop/src/link/Link.tsx
+++ packages/desktop/src/link/Link.tsx
@@ -1,6 +1,6 @@
 import React, {FC} from 'react'
 
-import {LinkControl, Lnk, Typo} from '@qiwi/pijma-core'
+import {LinkControl, Lnk, Typo, Stub} from '@qiwi/pijma-core'
 
 export interface LinkProps {
   onClick?: (href?: string, target?: string, download?: string | boolean, rel?: string) => void
@@ -15,12 +15,31 @@ export interface LinkProps {
   title?: string
   size?: 's' | 'm' | 'l'
   bold?: boolean
+  stub?: boolean
 }
 
 const LinkSize: { [size in NonNullable<LinkProps['size']>]: number } = {
   s: 3.5,
   m: 4,
   l: 5,
+}
+
+const StubLinkSize: Record<NonNullable<LinkProps['size']>, number> = {
+  s: 2,
+  m: 3,
+  l: 3,
+}
+
+const StubOffSet: Record<NonNullable<LinkProps['size']>, number> = {
+  s: 1.5,
+  m: 1.5,
+  l: 2.5,
+}
+
+const StubOffSetCompact: Record<NonNullable<LinkProps['size']>, number> = {
+  s: 1,
+  m: 1,
+  l: 2,
 }
 
 const LinkHeight: { [size in NonNullable<LinkProps['size']>]: number } = {
@@ -38,38 +57,51 @@ const LinkHeightCompact: { [size in NonNullable<LinkProps['size']>]: number } = 
 const TypoLink = Typo.withComponent(Lnk)
 
 export const Link: FC<LinkProps> = (props) => (
-  <LinkControl
-    onClick={props.onClick}
-    onFocus={props.onFocus}
-    onBlur={props.onBlur}
-    href={props.href}
-    target={props.target}
-    download={props.download}
-    rel={props.rel}
-    children={(renderProps) => (
-      <TypoLink
-        tabIndex={props.tabIndex}
-        href={props.href}
-        onClick={renderProps.onClick}
-        onFocus={renderProps.onFocus}
-        onBlur={renderProps.onBlur}
-        onMouseEnter={renderProps.onMouseEnter}
-        onMouseLeave={renderProps.onMouseLeave}
-        onMouseUp={renderProps.onMouseUp}
-        onMouseDown={renderProps.onMouseDown}
-        color={renderProps.hover || renderProps.focus ? '#FF8C00' : '#0055BB'}
-        transition="all 100ms cubic-bezier(0.4, 0.0, 0.2, 1)"
-        cursor="pointer"
-        decoration="none"
-        target={props.target}
-        download={props.download}
-        rel={props.rel}
-        title={props.title}
-        size={props.size === undefined ? undefined : props.compact ? LinkHeightCompact[props.size] : LinkSize[props.size]}
-        height={props.size === undefined ? undefined : LinkHeight[props.size]}
-        weight={props.bold === undefined ? undefined : props.bold ? 500 : 300}
-        children={props.children}
+  props.stub ? (
+    props.size === undefined ? (
+      null
+    ) : (
+      <Stub
+        top={props.compact ? StubOffSetCompact[props.size] : StubOffSet[props.size]}
+        bottom={props.compact ? StubOffSetCompact[props.size] : StubOffSet[props.size]}
+        height={StubLinkSize[props.size]}
+        width={1}
       />
-    )}
-  />
+    )
+  ) : (
+    <LinkControl
+      onClick={props.onClick}
+      onFocus={props.onFocus}
+      onBlur={props.onBlur}
+      href={props.href}
+      target={props.target}
+      download={props.download}
+      rel={props.rel}
+      children={(renderProps) => (
+        <TypoLink
+          tabIndex={props.tabIndex}
+          href={props.href}
+          onClick={renderProps.onClick}
+          onFocus={renderProps.onFocus}
+          onBlur={renderProps.onBlur}
+          onMouseEnter={renderProps.onMouseEnter}
+          onMouseLeave={renderProps.onMouseLeave}
+          onMouseUp={renderProps.onMouseUp}
+          onMouseDown={renderProps.onMouseDown}
+          color={renderProps.hover || renderProps.focus ? '#FF8C00' : '#0055BB'}
+          transition="all 100ms cubic-bezier(0.4, 0.0, 0.2, 1)"
+          cursor="pointer"
+          decoration="none"
+          target={props.target}
+          download={props.download}
+          rel={props.rel}
+          title={props.title}
+          size={props.size === undefined ? undefined : props.compact ? LinkHeightCompact[props.size] : LinkSize[props.size]}
+          height={props.size === undefined ? undefined : LinkHeight[props.size]}
+          weight={props.bold === undefined ? undefined : props.bold ? 500 : 300}
+          children={props.children}
+        />
+      )}
+    />
+  )
 )
