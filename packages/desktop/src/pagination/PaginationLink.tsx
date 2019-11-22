632f18d1a3f72119873fb72b4b5aa4c351ae7fef
AlekseyShatohin <56636950+AlekseyShatohin@users.noreply.github.com>
2019-11-22 11:36:15 +0300

    fix: PR fixes

--- packages/desktop/src/pagination/PaginationLink.tsx
+++ packages/desktop/src/pagination/PaginationLink.tsx
@@ -25,6 +25,7 @@ export const PaginationLink: FC<PaginationLinkProps> = props => (
     onClick={props.stub ? undefined : props.onClick}
     children={renderProps => (
       <CardLink
+        as={props.stub ? 'div' : 'a'}
         transition="all 100ms cubic-bezier(0.4, 0.0, 0.2, 1)"
         height={12}
         width={props.width}
