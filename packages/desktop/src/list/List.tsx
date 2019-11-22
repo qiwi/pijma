60c044cf5089e749c1007da74b16c6cfe2eaced2
AlekseyShatohin <56636950+AlekseyShatohin@users.noreply.github.com>
2019-11-22 10:51:32 +0300

    fix: PR fixes

--- packages/desktop/src/list/List.tsx
+++ packages/desktop/src/list/List.tsx
@@ -41,68 +41,37 @@ const LetterSize: { [size in NonNullable<ListProps['size']>]: number } = {
   m: 2.5,
   l: 3,
 }
-const ListItemStepStubYMargin: Record<NonNullable<ListProps['size']>, number> = {
-  s: 4,
-  m: 3,
-  l: 1,
-}
 
 export const List: FunctionComponent<ListProps> = ({stub = false, type, size = 'm', children}) => (
-  stub ? (
-    <Box>
-      {[0, 0].map((_width, index) => (
-        <Flex key={index} as="li" mt={index > 0 ? ItemIndent[type] : 0}>
-          {type === 'number' || type === 'bullet' ? (
-            <FlexItem>
+  <Box as={ListType[type]}>
+    {(stub ? [0, 0] : (children)).map((item, index, array) => (
+      <Flex key={index} as="li" mt={index > 0 ? ItemIndent[type] : 0}>
+        {type === 'number' ? (
+          stub ? (
+            <FlexItem mr={SpaceSize[size]} align="center">
               <Stub height={LetterSize[size]} width={LetterSize[size]} r={LetterSize[size] * 2}/>
             </FlexItem>
-          ) : type === 'step' ? (
-            <Flex direction="column" height="auto" mr={5}>
-              <FlexItem>
-                <Stub
-                  height={10}
-                  width={10}
-                  r={20}
-                />
-              </FlexItem>
-              {index === 1 ? (
-                null
-              ) : (
-                <FlexItem height={1} minHeight={3} align="center" justify="center" mt={3}>
-                  <Stub height={1} width="4px"/>
-                </FlexItem>
-              )}
-            </Flex>
           ) : (
-            null
-          )}
-          <Box my={type === 'step' ? ListItemStepStubYMargin[size] : 0} width={1}>
-            {type === 'number' || type === 'bullet' ? (
-              <Stub height={LetterSize[size]} width={33} left={3}/>
-            ) : type === 'step' ? (
-              <Paragraph size={size} compact stub/>
-            ) : (
-              null
-            )}
-          </Box>
-        </Flex>
-      ))}
-    </Box>
-  ) : (
-    <Box as={ListType[type]}>
-      {children.map((item, index) => (
-        <Flex key={index} as="li" mt={index > 0 ? ItemIndent[type] : 0}>
-          {type === 'number' ? (
             <FlexItem width={String(children.length).length * LetterSize[size] + SpaceSize[size]} shrink={0}>
               <Text size={size} bold={false}>{index + 1}.</Text>
             </FlexItem>
-          ) : type === 'bullet' ? (
+          )
+        ) : type === 'bullet' ? (
+          stub ? (
+            <FlexItem width={5} shrink={0} align="center">
+              <Stub height={LetterSize[size]} width={LetterSize[size]} r={LetterSize[size] * 2}/>
+            </FlexItem>
+          ) : (
             <FlexItem width={5} shrink={0}>
               <Text size={size} bold={false}>&#8226;</Text>
             </FlexItem>
-          ) : type === 'step' ? (
-            <Flex direction="column" height="auto" mr={5}>
-              <FlexItem shrink={0}>
+          )
+        ) : type === 'step' ? (
+          <Flex direction="column" height="auto" mr={5}>
+            <FlexItem shrink={0}>
+              {stub ? (
+                <Stub height={10} width={10} r={20}/>
+              ) : (
                 <Card bg="#F5F5F5" r="50%" height={10} width={10}>
                   <Flex align="center" justify="center" height={1} width={1}>
                     <Text size={size} bold>
@@ -110,28 +79,39 @@ export const List: FunctionComponent<ListProps> = ({stub = false, type, size = '
                     </Text>
                   </Flex>
                 </Card>
-              </FlexItem>
-              {index + 1 === children.length ? (
-                null
-              ) : (
-                <FlexItem height={1} minHeight={3} align="center" justify="center" mt={3}>
-                  <Card bg="#F5F5F5" height={1} width="4px"/>
-                </FlexItem>
               )}
-            </Flex>
+            </FlexItem>
+            {index + 1 === array.length ? (
+              null
+            ) : (
+              <FlexItem height={1} minHeight={3} align="center" justify="center" mt={3}>
+                {stub ? (
+                  <Stub height={1} width="4px"/>
+                ) : (
+                  <Card bg="#F5F5F5" height={1} width="4px"/>
+                )}
+              </FlexItem>
+            )}
+          </Flex>
+        ) : (
+          null
+        )}
+        <Box my={ListItemYMargin[type]} width={1}>
+          {stub ? (
+            (type === 'step' ? [0.8, 0.9, 0.7] : [33]).map((width, index) => (
+              <Box width={width} key={index}>
+                <Text display="block" size={size} stub/>
+              </Box>
+            ))
           ) : (
-            null
-          )}
-          <Box my={ListItemYMargin[type]}>
-            {typeof item === 'string' ? (
+            typeof item === 'string' ? (
               <Paragraph size={size}>{item}</Paragraph>
             ) : (
               item
-            )}
-          </Box>
-        </Flex>
-      ))}
-    </Box>
-  )
-
+            )
+          )}
+        </Box>
+      </Flex>
+    ))}
+  </Box>
 )
