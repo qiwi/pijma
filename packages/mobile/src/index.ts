6c0d4d5ba00ad02738638fd37e4d20d50d4b3e13
irinaa133 <irina.soldatenko156@mail.ru>
2019-12-02 11:52:13 +0300

    feat: export suggest-field

--- packages/mobile/src/index.ts
+++ packages/mobile/src/index.ts
@@ -30,6 +30,7 @@ export * from './rating'
 export * from './content-suggest'
 export * from './progress-bar'
 export * from './alert'
+export * from './suggest-field'
 
 export {TextField as MaskTextField} from './text-field'
 export {PasswordField as MaskPasswordField} from './password-field'
