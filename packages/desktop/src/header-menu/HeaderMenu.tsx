99bd455feb3d059af38313a00a3eae2ae68e9cfb
AlekseyShatohin <56636950+AlekseyShatohin@users.noreply.github.com>
2019-12-03 17:31:27 +0300

    fix: PR fixes

--- packages/desktop/src/header-menu/HeaderMenu.tsx
+++ packages/desktop/src/header-menu/HeaderMenu.tsx
@@ -12,19 +12,11 @@ export interface HeaderMenuProps {
 export const HeaderMenu: FC<HeaderMenuProps> = (props) => (
   <Flex height={1} width={1} justify="flex-start">
     {props.stub ? (
-      typeof props.stub === 'boolean' ? (
-        [32, 44, 26].map((width, index) => (
-          <FlexItem key={index} ml={index > 0 ? 6 : 0} width={width} align="center">
-            <Link size="m" stub/>
-          </FlexItem>
-        ))
-      ) : (
-        props.stub.map((width, index) => (
-          <FlexItem key={index} ml={index > 0 ? 6 : 0} width={width} align="center">
-            <Link size="m" stub/>
-          </FlexItem>
-        ))
-      )
+      (typeof props.stub === 'boolean' ? [32, 44, 26] : props.stub).map((width, index) => (
+        <FlexItem key={index} ml={index > 0 ? 6 : 0} width={width} align="center">
+          <Link size="m" stub/>
+        </FlexItem>
+      ))
     ) : (
       props.children.map((item, i) => (
         <FlexItem key={i} ml={i > 0 ? 6 : 0}>
