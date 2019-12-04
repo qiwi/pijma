6eb26c4c303423b33c33b0ace2c011efd4e95ac1
AlekseyShatohin <56636950+AlekseyShatohin@users.noreply.github.com>
2019-12-04 14:09:54 +0300

    fix: PR fixes

--- packages/mobile/src/footer-nav/FooterNav.tsx
+++ packages/mobile/src/footer-nav/FooterNav.tsx
@@ -13,19 +13,11 @@ export const FooterNav: FC<FooterNavProps> = ({children = [], stub = false}) => 
   <Box overflow="hidden">
     <Flex wrap="wrap" mx={-2} my={-2.5}>
       {stub ? (
-        typeof stub === 'boolean' ? (
-          [24, 30, 18].map((width, i) => (
-            <FlexItem px={2} py={2.5} key={i} width={width}>
-              <NavLink stub/>
-            </FlexItem>
-          ))
-        ) : (
-          stub.map((width, i) => (
-            <FlexItem px={2} py={2.5} key={i} width={width}>
-              <NavLink stub/>
-            </FlexItem>
-          ))
-        )
+        (typeof stub === 'boolean' ? [24, 30, 18] : stub).map((width, index) => (
+          <FlexItem px={2} py={2.5} key={index} width={width}>
+            <NavLink stub/>
+          </FlexItem>
+        ))
       ) : (
         children.map((item, i) => (
           <FlexItem px={2} py={2.5} key={i}>
