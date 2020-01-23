5547664e417be7bdda66aca7d213a812703b8a4b
Irinaa133 <33098238+Irinaa133@users.noreply.github.com>
2020-01-23 10:06:15 +0300

    feat: suggest-control items undefined

--- packages/core/src/suggest/SuggestControlState.ts
+++ packages/core/src/suggest/SuggestControlState.ts
@@ -2,5 +2,4 @@ export default interface SuggestControlState {
   show: boolean
   focused: boolean
   hovered: boolean
-  result: boolean
 }
--- packages/desktop/src/header-suggest/HeaderSuggestOptionModel.ts
+++ packages/desktop/src/header-suggest/HeaderSuggestOptionModel.ts
@@ -1,6 +1,6 @@
 import {SuggestOptionModel} from '@qiwi/pijma-core'
 
-export default interface HeaderSuggestOptionsModel<V> extends SuggestOptionModel<V> {
+export interface HeaderSuggestOptionModel<V> extends SuggestOptionModel<V> {
   title: string
   logo: string
   description: string
