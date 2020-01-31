c4fc9f5b37586f0b6c52931f8ab4d02b93a32255
AlekseyShatohin <56636950+AlekseyShatohin@users.noreply.github.com>
2020-01-31 13:36:36 +0300

    feat: stub menu-link

--- packages/mobile/src/menu/MenuLink.tsx
+++ packages/mobile/src/menu/MenuLink.tsx
@@ -15,6 +15,8 @@ export interface MenuLinkProps {
   target?: LinkControlProps['target']
   download?: LinkControlProps['download']
   rel?: LinkControlProps['rel']
+  size?: 's' | 'm'
+  stub?: boolean
   onClick?: LinkControlProps['onClick']
   onFocus?: LinkControlProps['onFocus']
   onBlur?: LinkControlProps['onBlur']
@@ -23,38 +25,49 @@ export interface MenuLinkProps {
 const MenuItemLnk = styled(Lnk)().withComponent(MenuItem)
 
 export const MenuLink: FC<MenuLinkProps> = (props) => (
-  <LinkControl
-    href={props.href}
-    target={props.target}
-    download={props.download}
-    rel={props.rel}
-    onClick={props.onClick}
-    onFocus={props.onFocus}
-    onBlur={props.onBlur}
-    children={(renderProps) => (
-      <MenuItemLnk
-        as={props.href ? 'a' : undefined}
-        notes={props.notes}
-        text={props.title}
-        icon={props.icon}
-        submenu={props.submenu}
-        active={props.active ? props.active : renderProps.active}
-        hover={renderProps.hover}
-        focus={renderProps.focus}
-        tabIndex={props.tabIndex}
-        href={props.href}
-        title={props.href ? props.title : undefined}
-        target={props.href ? props.target : undefined}
-        download={props.href ? props.download : undefined}
-        rel={props.href ? props.rel : undefined}
-        onClick={renderProps.onClick}
-        onFocus={renderProps.onFocus}
-        onBlur={renderProps.onBlur}
-        onMouseEnter={renderProps.onMouseEnter}
-        onMouseLeave={renderProps.onMouseLeave}
-        onMouseUp={renderProps.onMouseUp}
-        onMouseDown={renderProps.onMouseDown}
-      />
-    )}
-  />
+  props.stub ? (
+    <MenuItem
+      stub
+      text={props.title}
+      notes={props.notes}
+      icon={props.icon}
+      submenu={props.submenu}
+      size={props.size}
+    />
+  ) : (
+    <LinkControl
+      href={props.href}
+      target={props.target}
+      download={props.download}
+      rel={props.rel}
+      onClick={props.onClick}
+      onFocus={props.onFocus}
+      onBlur={props.onBlur}
+      children={(renderProps) => (
+        <MenuItemLnk
+          as={props.href ? 'a' : undefined}
+          notes={props.notes}
+          text={props.title}
+          icon={props.icon}
+          submenu={props.submenu}
+          active={props.active ? props.active : renderProps.active}
+          hover={renderProps.hover}
+          focus={renderProps.focus}
+          tabIndex={props.tabIndex}
+          href={props.href}
+          title={props.href ? props.title : undefined}
+          target={props.href ? props.target : undefined}
+          download={props.href ? props.download : undefined}
+          rel={props.href ? props.rel : undefined}
+          onClick={renderProps.onClick}
+          onFocus={renderProps.onFocus}
+          onBlur={renderProps.onBlur}
+          onMouseEnter={renderProps.onMouseEnter}
+          onMouseLeave={renderProps.onMouseLeave}
+          onMouseUp={renderProps.onMouseUp}
+          onMouseDown={renderProps.onMouseDown}
+        />
+      )}
+    />
+  )
 )
