3522347cdd23cf7327305d5c1e26b51d5ced2881
irinaa133 <irina.soldatenko156@mail.ru>
2020-02-03 12:48:35 +0300

    style: stub

--- packages/desktop/src/footer-app/FooterApp.tsx
+++ packages/desktop/src/footer-app/FooterApp.tsx
@@ -11,41 +11,46 @@ interface FooterAppLinkProps {
   rel?: string
   title?: string
   icon: ReactElement
+  stub?: boolean
   onClick?: (href?: string, target?: string, download?: string | boolean, rel?: string) => void
   onFocus?: () => void
   onBlur?: () => void
 }
 
 const FooterAppLink: FC<FooterAppLinkProps> = (props) => (
-  <LinkControl
-    href={props.href}
-    target={props.target}
-    download={props.download}
-    rel={props.rel}
-    onClick={props.onClick}
-    onFocus={props.onFocus}
-    onBlur={props.onBlur}
-    children={(renderProps) => (
-      <BoxLink
-        display="block"
-        href={props.href}
-        rel={props.rel}
-        target={props.target}
-        title={props.title}
-        download={props.download}
-        width={1}
-        height={1}
-        onClick={renderProps.onClick}
-        onFocus={renderProps.onFocus}
-        onBlur={renderProps.onBlur}
-        onMouseEnter={renderProps.onMouseEnter}
-        onMouseLeave={renderProps.onMouseLeave}
-        onMouseUp={renderProps.onMouseUp}
-        onMouseDown={renderProps.onMouseDown}
-        children={props.icon}
-      />
-    )}
-  />
+  props.stub ? (
+    <Stub width={39} height={12}/>
+  ) : (
+    <LinkControl
+      href={props.href}
+      target={props.target}
+      download={props.download}
+      rel={props.rel}
+      onClick={props.onClick}
+      onFocus={props.onFocus}
+      onBlur={props.onBlur}
+      children={(renderProps) => (
+        <BoxLink
+          display="block"
+          href={props.href}
+          rel={props.rel}
+          target={props.target}
+          title={props.title}
+          download={props.download}
+          width={1}
+          height={1}
+          onClick={renderProps.onClick}
+          onFocus={renderProps.onFocus}
+          onBlur={renderProps.onBlur}
+          onMouseEnter={renderProps.onMouseEnter}
+          onMouseLeave={renderProps.onMouseLeave}
+          onMouseUp={renderProps.onMouseUp}
+          onMouseDown={renderProps.onMouseDown}
+          children={props.icon}
+        />
+      )}
+    />
+  )
 )
 
 export interface FooterAppProps {
@@ -63,11 +68,7 @@ export const FooterApp: FC<FooterAppProps> = ({children, stub = false}) => (
           maxWidth={42}
           maxHeight={12.4}
           m={2}
-          children={stub ? (
-            <Stub width={39} height={12}/>
-          ) : (
-            <FooterAppLink {...item}/>
-          )}
+          children={<FooterAppLink stub={stub} {...item}/>}
         />
       ))}
     </Flex>
