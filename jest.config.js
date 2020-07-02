8268de59015597d693982ea425523fa8a6584f86
irinaa133 <irina.soldatenko156@mail.ru>
2020-07-02 09:18:25 +0300

    fix: remove global jest-config

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  projects: [
-    '<rootDir>/packages/core/jest.config.js',
-    '<rootDir>/packages/desktop/jest.config.js',
-    '<rootDir>/packages/mobile/jest.config.js',
-  ],
-}
