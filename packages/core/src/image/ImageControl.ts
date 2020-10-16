1efae1f0e58dd9edfe465c5f2d9498ff4fd347f6
AlekseyShatohin <56636950+AlekseyShatohin@users.noreply.github.com>
2020-10-16 09:51:44 +0300

    fix: load throw timeouts

--- packages/core/src/image/ImageControl.ts
+++ packages/core/src/image/ImageControl.ts
@@ -22,9 +22,10 @@ export interface ImageControlProps {
 }
 
 export interface ImageControlState {
-  viewed: boolean
-  loaded: boolean
-  cached: boolean | undefined
+  view: boolean
+  load: boolean
+  done: boolean
+  cache: boolean | undefined
 }
 
 export class ImageControl extends Component<ImageControlProps, ImageControlState> {
@@ -35,9 +36,10 @@ export class ImageControl extends Component<ImageControlProps, ImageControlState
   }
 
   public state: ImageControlState = {
-    viewed: false,
-    loaded: false,
-    cached: undefined,
+    view: false,
+    load: false,
+    done: false,
+    cache: undefined,
   }
 
   public componentWillUnmount: () => void = () => {
@@ -49,69 +51,63 @@ export class ImageControl extends Component<ImageControlProps, ImageControlState
 
   private cachedTimer: number | undefined
 
-  private image: HTMLImageElement | undefined
-
   private onChange: (inView: boolean) => void = (inView) => {
+    this.setState({
+      view: inView,
+    })
     clearTimeout(this.viewedTimer)
     if (!inView) {
-      if (this.image) {
-        this.image.src = ''
-        this.image.srcset = ''
-      }
       return
     }
-    if (this.state.cached === undefined) {
-      this.image = document.createElement('img')
-      this.image.src = this.props.src
-      if (this.props.srcSet) {
-        this.image.srcset = this.props.srcSet
-      }
-      this.image.onload = () => {
+    if (this.state.cache === undefined) {
+      this.setState({
+        cache: true,
+      })
+      const image = document.createElement('img')
+      image.src = this.props.src
+      image.srcset = this.props.srcSet || ''
+      image.onload = () => {
+        clearTimeout(this.viewedTimer)
         this.setState({
-          cached: true,
-          viewed: true,
+          load: true,
+          cache: false,
         })
       }
       this.cachedTimer = setTimeout(() => {
-        if (!this.state.cached && this.image) {
-          this.image.onload = null
-          this.setState({
-            cached: false,
-          })
-          this.onView()
-        }
+        image.onload = null
+        image.src = ''
+        image.srcset = ''
+        this.setState({
+          cache: false,
+        })
       }, this.props.cachedDelay)
-      return
     }
-    this.onView()
-  }
-
-  private onView: () => void = () => {
     this.viewedTimer = setTimeout(() => {
       this.setState({
-        viewed: true,
+        load: true,
       })
     }, this.props.viewedDelay)
   }
+
   private onLoad: () => void = () => {
-    if (!this.state.viewed) {
+    if (!this.state.load) {
       return
     }
     if (this.props.onLoad) {
       this.props.onLoad()
     }
     this.setState({
-      loaded: true,
+      done: true,
     })
   }
 
   private get src(): string | undefined {
     const {stub, src} = this.props
-    if (this.state.cached === undefined) {
-      return undefined
-    }
-    if (this.state.viewed) {
+    if (this.state.load) {
       return src
+    }
+    if (this.state.cache || !this.state.view) {
+      return undefined
     }
     if (typeof stub === 'string') {
       return stub
@@ -120,17 +116,17 @@ export class ImageControl extends Component<ImageControlProps, ImageControlState
   }
 
   private get srcSet(): string | undefined {
-    if (!this.state.viewed || this.state.cached === undefined) {
-      return undefined
+    if (this.state.load) {
+      return this.props.srcSet
     }
-    return this.props.srcSet
+    return undefined
   }
 
   public render() {
     return this.props.children({
       src: this.src,
       srcSet: this.srcSet,
-      loaded: this.state.loaded,
+      loaded: this.state.done,
       onChange: this.onChange,
       onLoad: this.onLoad,
     })
