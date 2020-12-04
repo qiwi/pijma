7106e0a1f7c4d68d54e3353518f419aa4edc7fae
Margarita Yanochkina <yanochkina.mv@gmail.com>
2020-12-04 15:47:58 +0300

    feat: simplified

--- packages/desktop/src/annotation/Annotation.tsx
+++ packages/desktop/src/annotation/Annotation.tsx
@@ -1,73 +1,12 @@
 import React, {FC, ReactElement} from 'react'
-import {Striper, Card, Box, Spacer} from '@qiwi/pijma-core'
-import {Heading, Paragraph} from '../typography'
-
-type AnnotationText = ReactElement | string
-type AnnotationItem = {itemContent: AnnotationText, itemTitle?: AnnotationText}
+import {Card} from '@qiwi/pijma-core'
 
 export interface AnnotationProps {
-  children: {
-    content?: AnnotationText,
-    title?: AnnotationText,
-    items?: AnnotationItem[]
-  }
+  children: ReactElement | string
 }
 
-export const Annotation: FC<AnnotationProps> = ({children}) => {
-  if (!children.items) {
-    return (
-      <Card px={11} pt={10} pb={11} r={10} b="solid 1px #e6e6e6">
-        <AnnotationTop content={children.content!} title={children.title}/>
-      </Card>
-    )
-  }
-  if (!children.title && !children.content) {
-    return (
-      <Card px={11} pt={10} pb={11} r={10} b="solid 1px #e6e6e6">
-        <AnnotationBottom items={children.items}/>
-      </Card>
-    )
-  }
-  return (
-    <Card px={11} pt={10} pb={11} r={10} b="solid 1px #e6e6e6">
-      <Striper>
-        <Box pb={5}>
-          <AnnotationTop content={children.content!} title={children.title}/>
-        </Box>
-        <Box pt={5}>
-          <AnnotationBottom items={children.items}/>
-        </Box>
-      </Striper>
-    </Card>
-  )
-}
-
-const AnnotationTop: FC<{content: AnnotationText, title?: AnnotationText}> = (props) => (
-  <Spacer size="xxs">
-    {props.title ? (
-      <Heading size="4">
-        {props.title}
-      </Heading>
-    ) : null}
-    <Paragraph>
-      {props.content}
-    </Paragraph>
-  </Spacer>
-)
-
-const AnnotationBottom: FC<{items: AnnotationItem[]}> = (props) => (
-  <Spacer size="m">
-    {props.items.map((item, key) => (
-      <Spacer size="xxs" key={key}>
-        {item.itemTitle ? (
-          <Heading size="5">
-            {item.itemTitle}
-          </Heading>
-        ) : null}
-        <Paragraph>
-          {item.itemContent}
-        </Paragraph>
-      </Spacer>
-    ))}
-  </Spacer>
+export const Annotation: FC<AnnotationProps> = ({children}) => (
+  <Card px={11} pt={11} pb={12} r={10} b="solid 1px #e6e6e6">
+    {children}
+  </Card>
 )
