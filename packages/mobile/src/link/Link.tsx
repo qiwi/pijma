cc9c9d4685f907e6441bbc12bdf733125fa7425e
AlekseyShatohin <56636950+AlekseyShatohin@users.noreply.github.com>
2021-01-13 16:06:54 +0400

    fix: PR fixes

--- packages/mobile/src/link/Link.tsx
+++ packages/mobile/src/link/Link.tsx
@@ -89,7 +89,7 @@ export const Link: FC<LinkProps> = (props) => (
           onMouseLeave={renderProps.onMouseLeave}
           onMouseUp={renderProps.onMouseUp}
           onMouseDown={renderProps.onMouseDown}
-          color={(renderProps.hover || renderProps.focus) ? props.inverse ? '#CCC' : '#FF8C00' : props.inverse ? '#FFF' : '#0055BB'}
+          color={renderProps.hover || renderProps.focus ? props.inverse ? '#CCC' : '#FF8C00' : props.inverse ? '#FFF' : '#0055BB'}
           transition="all 100ms cubic-bezier(0.4, 0.0, 0.2, 1)"
           cursor="pointer"
           decoration="none"
