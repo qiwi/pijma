2e81593b9d35aa9d638b81d33d6fcb4d9f194d16
an-kharitonov <an.kharitonov@qiwi.com>
2021-03-02 18:29:11 +0300

    feat: add stub to Glossary

--- packages/desktop/src/glossary/Glossary.tsx
+++ packages/desktop/src/glossary/Glossary.tsx
@@ -6,9 +6,42 @@ type GlossaryText = ReactElement | string
 
 export interface GlossaryProps {
   children: {title: GlossaryText, content: GlossaryText | GlossaryText[]}[]
+  stub?: boolean,
 }
 
-export const Glossary: FC<GlossaryProps> = ({children}) => (
+export const Glossary: FC<GlossaryProps> = ({children, stub}) => stub ? (
+  <Box as="dl">
+    {children.map((item, i) => (
+      <Box
+        key={i}
+        mt={i === 0 ? undefined : 5}
+      >
+        <Box as="dt" maxWidth={22}>
+          <Text
+            stub
+            display="block"
+            bold={false}
+            compact
+            size="s"
+          />
+        </Box>
+        <Box mt={1} as="dd" maxWidth={32}>
+          {([] as GlossaryText[]).concat(item.content).map((_content, j) => (
+            <Box key={`${i}.${j}`} mt={j === 0 ? undefined : 2}>
+              <Text
+                stub
+                bold={false}
+                compact
+                display="block"
+                size="m"
+              />
+            </Box>
+          ))}
+        </Box>
+      </Box>
+    ))}
+  </Box>
+) : (
   <Box as="dl">
     {children.map((item, i) => (
       <Box
@@ -19,6 +52,7 @@ export const Glossary: FC<GlossaryProps> = ({children}) => (
           <Text
             bold={false}
             compact
+            display="block"
             size="s"
             color="support"
             children={item.title}
@@ -30,6 +64,7 @@ export const Glossary: FC<GlossaryProps> = ({children}) => (
               <Text
                 bold={false}
                 compact
+                display="block"
                 size="m"
                 children={content}
               />
--- packages/mobile/src/glossary/Glossary.tsx
+++ packages/mobile/src/glossary/Glossary.tsx
@@ -6,9 +6,42 @@ type GlossaryText = ReactElement | string
 
 export interface GlossaryProps {
   children: {title: GlossaryText, content: GlossaryText | GlossaryText[]}[]
+  stub?: boolean,
 }
 
-export const Glossary: FC<GlossaryProps> = ({children}) => (
+export const Glossary: FC<GlossaryProps> = ({children, stub}) => stub ? (
+  <Box as="dl">
+    {children.map((item, i) => (
+      <Box
+        key={i}
+        mt={i === 0 ? undefined : 5}
+      >
+        <Box as="dt" maxWidth={22}>
+          <Text
+            stub
+            display="block"
+            bold={false}
+            compact
+            size="s"
+          />
+        </Box>
+        <Box as="dd" mt={1} maxWidth={32}>
+          {([] as GlossaryText[]).concat(item.content).map((_content, j) => (
+            <Box key={`${i}.${j}`} mt={j === 0 ? undefined : 2}>
+              <Text
+                stub
+                bold={false}
+                display="block"
+                compact
+                size="m"
+              />
+            </Box>
+          ))}
+        </Box>
+      </Box>
+    ))}
+  </Box>
+) : (
   <Box as="dl">
     {children.map((item, i) => (
       <Box
@@ -17,6 +50,7 @@ export const Glossary: FC<GlossaryProps> = ({children}) => (
       >
         <Box as="dt">
           <Text
+            display="block"
             bold={false}
             compact
             size="s"
@@ -28,6 +62,7 @@ export const Glossary: FC<GlossaryProps> = ({children}) => (
           {([] as GlossaryText[]).concat(item.content).map((content, j) => (
             <Box key={`${i}.${j}`} mt={j === 0 ? undefined : 2}>
               <Text
+                display="block"
                 bold={false}
                 compact
                 size="m"
