b87a56709a35f754993f972303c7749c8d5c6d05
an-kharitonov <an.kharitonov@qiwi.com>
2021-03-03 13:38:20 +0300

    fix: coding style

--- packages/desktop/src/glossary/Glossary.tsx
+++ packages/desktop/src/glossary/Glossary.tsx
@@ -9,71 +9,73 @@ export interface GlossaryProps {
   stub?: boolean,
 }
 
-export const Glossary: FC<GlossaryProps> = ({children, stub}) => stub ? (
-  <Box as="dl">
-    {children.map((item, i) => (
-      <Box
-        key={i}
-        mt={i === 0 ? undefined : 5}
-      >
-        <Box as="dt" mr={16} minWidth={0.5}>
-          <Text
-            stub
-            display="block"
-            bold={false}
-            compact
-            size="s"
-          />
+export const Glossary: FC<GlossaryProps> = ({children, stub}) => (
+  stub ? (
+    <Box as="dl">
+      {children.map((item, i) => (
+        <Box
+          key={i}
+          mt={i === 0 ? undefined : 5}
+        >
+          <Box as="dt" mr={16}>
+            <Text
+              stub
+              display="block"
+              bold={false}
+              compact
+              size="s"
+            />
+          </Box>
+          <Box mt={1} as="dd">
+            <Spacer size="xs">
+              {(Array.isArray(item.content) ? item.content : [item.content]).map((_content, j) => (
+                <Text
+                  key={`${i}.${j}`}
+                  stub
+                  bold={false}
+                  compact
+                  display="block"
+                  size="m"
+                />
+              ))}
+            </Spacer>
+          </Box>
         </Box>
-        <Box mt={1} as="dd">
-          <Spacer size="xs">
-            {(Array.isArray(item.content) ? item.content : [item.content]).map((_content, j) => (
-              <Text
-                key={`${i}.${j}`}
-                stub
-                bold={false}
-                compact
-                display="block"
-                size="m"
-              />
-            ))}
-          </Spacer>
+      ))}
+    </Box>
+  ) : (
+    <Box as="dl">
+      {children.map((item, i) => (
+        <Box
+          key={i}
+          mt={i === 0 ? undefined : 5}
+        >
+          <Box as="dt">
+            <Text
+              bold={false}
+              compact
+              display="block"
+              size="s"
+              color="support"
+              children={item.title}
+            />
+          </Box>
+          <Box mt={1} as="dd">
+            <Spacer size="xs">
+              {(Array.isArray(item.content) ? item.content : [item.content]).map((content, j) => (
+                <Text
+                  key={`${i}.${j}`}
+                  bold={false}
+                  compact
+                  display="block"
+                  size="m"
+                  children={content}
+                />
+              ))}
+            </Spacer>
+          </Box>
         </Box>
-      </Box>
-    ))}
-  </Box>
-) : (
-  <Box as="dl">
-    {children.map((item, i) => (
-      <Box
-        key={i}
-        mt={i === 0 ? undefined : 5}
-      >
-        <Box as="dt">
-          <Text
-            bold={false}
-            compact
-            display="block"
-            size="s"
-            color="support"
-            children={item.title}
-          />
-        </Box>
-        <Box mt={1} as="dd">
-          <Spacer size="xs">
-            {(Array.isArray(item.content) ? item.content : [item.content]).map((content, j) => (
-              <Text
-                key={`${i}.${j}`}
-                bold={false}
-                compact
-                display="block"
-                size="m"
-                children={content}
-              />
-            ))}
-          </Spacer>
-        </Box>
-      </Box>
-    ))}
-  </Box>
+      ))}
+    </Box>
+  )
 )
--- packages/mobile/src/glossary/Glossary.tsx
+++ packages/mobile/src/glossary/Glossary.tsx
@@ -9,71 +9,73 @@ export interface GlossaryProps {
   stub?: boolean,
 }
 
-export const Glossary: FC<GlossaryProps> = ({children, stub}) => stub ? (
-  <Box as="dl">
-    {children.map((item, i) => (
-      <Box
-        key={i}
-        mt={i === 0 ? undefined : 5}
-      >
-        <Box as="dt" mr={13} minWidth={0.5}>
-          <Text
-            stub
-            display="block"
-            bold={false}
-            compact
-            size="s"
-          />
+export const Glossary: FC<GlossaryProps> = ({children, stub}) => (
+  stub ? (
+    <Box as="dl">
+      {children.map((item, i) => (
+        <Box
+          key={i}
+          mt={i === 0 ? undefined : 5}
+        >
+          <Box as="dt" mr={13} minWidth={0.5}>
+            <Text
+              stub
+              display="block"
+              bold={false}
+              compact
+              size="s"
+            />
+          </Box>
+          <Box as="dd" mt={1}>
+            <Spacer size="xs">
+              {(Array.isArray(item.content) ? item.content : [item.content]).map((_content, j) => (
+                <Text
+                  key={`${i}.${j}`}
+                  stub
+                  bold={false}
+                  display="block"
+                  compact
+                  size="m"
+                />
+              ))}
+            </Spacer>
+          </Box>
         </Box>
-        <Box as="dd" mt={1}>
-          <Spacer size="xs">
-            {(Array.isArray(item.content) ? item.content : [item.content]).map((_content, j) => (
-              <Text
-                key={`${i}.${j}`}
-                stub
-                bold={false}
-                display="block"
-                compact
-                size="m"
-              />
-            ))}
-          </Spacer>
+      ))}
+    </Box>
+  ) : (
+    <Box as="dl">
+      {children.map((item, i) => (
+        <Box
+          key={i}
+          mt={i === 0 ? undefined : 5}
+        >
+          <Box as="dt">
+            <Text
+              display="block"
+              bold={false}
+              compact
+              size="s"
+              color="support"
+              children={item.title}
+            />
+          </Box>
+          <Box mt={1} as="dd">
+            <Spacer size="xs">
+              {(Array.isArray(item.content) ? item.content : [item.content]).map((content, j) => (
+                <Text
+                  key={`${i}.${j}`}
+                  display="block"
+                  bold={false}
+                  compact
+                  size="m"
+                  children={content}
+                />
+              ))}
+            </Spacer>
+          </Box>
         </Box>
-      </Box>
-    ))}
-  </Box>
-) : (
-  <Box as="dl">
-    {children.map((item, i) => (
-      <Box
-        key={i}
-        mt={i === 0 ? undefined : 5}
-      >
-        <Box as="dt">
-          <Text
-            display="block"
-            bold={false}
-            compact
-            size="s"
-            color="support"
-            children={item.title}
-          />
-        </Box>
-        <Box mt={1} as="dd">
-          <Spacer size="xs">
-            {(Array.isArray(item.content) ? item.content : [item.content]).map((content, j) => (
-              <Text
-                key={`${i}.${j}`}
-                display="block"
-                bold={false}
-                compact
-                size="m"
-                children={content}
-              />
-            ))}
-          </Spacer>
-        </Box>
-      </Box>
-    ))}
-  </Box>
+      ))}
+    </Box>
+  )
 )
