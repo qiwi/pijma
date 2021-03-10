87dc7f5449893e20968dd9f54d338dc24235de33
Anton Golub <golub.anton@gmail.com>
2021-03-10 20:07:58 +0300

    perf: migrate to @qiwi/semrel-toolkit (#377)

---
+++ packages/core/.releaserc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  branch: 'master',
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    [
+      '@semantic-release/github',
+      {
+        successComment: false,
+        failComment: false
+      }
+    ],
+    '@semantic-release/git'
+  ]
+}
