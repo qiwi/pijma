90a02c6561788dc4cec7722c4f52b61eec6c78b4
Anton Golub <mailbox@antongolub.ru>
2021-03-10 19:22:01 +0300

    ci: migrate to @qiwi/semrel-toolkit

---
+++ packages/mobile/.releaserc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  branch: 'master',
+  plugins: [
+    [
+      '@qiwi/semrel-metabranch',
+      {
+        publish: {
+          action: 'push',
+          branch: 'docs',
+          from: './styleguide/lib',
+          to: './mobile',
+        }
+      }
+    ],
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    [
+      '@semantic-release/github',
+      {
+        successComment: false,
+        failComment: false
+      }
+    ],
+    '@semantic-release/git'
+  ]
+}
