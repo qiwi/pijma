7998643b4920d1ea36e0881a1e7df79132ea9451
Anton Golub <antongolub@antongolub.com>
2021-12-15 13:53:51 +0300

    build: move to es6 and esbuild (#459)

--- packages/desktop/styleguide/require.js
+++ packages/desktop/styleguide/require.js
@@ -1,5 +1,4 @@
 import * as core from '@qiwi/pijma-core'
 import * as desktop from '@qiwi/pijma-desktop'
 
-Object.keys(core).forEach(name => global[name] = core[name])
-Object.keys(desktop).forEach(name => global[name] = desktop[name])
+Object.assign(global, core, desktop)
