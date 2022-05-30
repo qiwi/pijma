0100d91f429967237c38f19df8a4f0e4bdce2a2e
Dima Livshits <dimonka83@gmail.com>
2022-05-30 12:32:13 +0300

    fix: ssr releaser

--- packages/ssr/.releaserc.cjs
+++ packages/ssr/.releaserc.cjs
@@ -11,26 +11,31 @@ module.exports = {
           {type: 'refactor', release: 'patch'},
         ],
         parserOpts: {
-          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES'],
-        },
-      },
+          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES']
+        }
+      }
     ],
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
-    '@semantic-release/npm',
+    '@semrel-extra/npm',
+    [
+      '@semantic-release/exec',
+      {
+        prepareCmd: 'YARN_ENABLE_IMMUTABLE_INSTALLS=false yarn install && git add ../../yarn.lock'
+      }
+    ],
     [
       '@semantic-release/github',
       {
         successComment: false,
-        failComment: false,
-      },
+        failComment: false
+      }
     ],
     [
       '@semantic-release/git',
       {
-        message:
-          'chore(release): ${nextRelease.gitTag} [skip ci]\n\n${nextRelease.notes}',
-      },
-    ],
-  ],
+        message: 'chore(release): ${nextRelease.gitTag} [skip ci]\n\n${nextRelease.notes}'
+      }
+    ]
+  ]
 }
