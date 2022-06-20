a2b48534c7ae9266420ffc255c65174e8ee32a67
Dima Livshits <dimonka83@gmail.com>
2022-06-20 16:47:44 +0300

    fix: deps

--- packages/infra/.releaserc.js
+++ packages/infra/.releaserc.js
@@ -1,10 +1,29 @@
 module.exports = {
   branch: 'master',
   plugins: [
-    '@semantic-release/commit-analyzer',
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'angular',
+        releaseRules: [
+          {type: 'docs', release: 'patch'},
+          {type: 'doc', release: 'patch'},
+          {type: 'refactor', release: 'patch'},
+        ],
+        parserOpts: {
+          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES']
+        }
+      }
+    ],
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
-    '@semantic-release/npm',
+    '@semrel-extra/npm',
+    [
+      '@semantic-release/exec',
+      {
+        prepareCmd: 'YARN_ENABLE_IMMUTABLE_INSTALLS=false yarn install && git add ../../yarn.lock'
+      }
+    ],
     [
       '@semantic-release/github',
       {
