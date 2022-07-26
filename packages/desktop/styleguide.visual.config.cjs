57b460a3a8511968e0becbb7bea422cf4ebc6cdb
Dima Livshits <dimonka83@gmail.com>
2022-07-26 18:33:30 +0300

    fix: rename config

---
+++ packages/desktop/styleguide.visual.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  wait: 50,
+  threshold: 0.1,
+  viewports: {
+    desktop: {
+      width: 1200,
+      height: 900,
+    },
+  },
+  deleteScreenshotsWhenAccepted: true,
+  filter: [
+    'ReCaptchaField',
+  ],
+}
---
+++ packages/mobile/styleguide.visual.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  wait: 50,
+  threshold: 0.1,
+  viewports: {
+    mobile: {
+      width: 400,
+      height: 800,
+    },
+  },
+  deleteScreenshotsWhenAccepted: true,
+  filter: [
+    'ReCaptchaField',
+  ],
+}
