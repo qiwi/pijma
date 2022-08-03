9a74056f0916440965f894a6314f4a1820ffe7dd
Екатерина Никишина <ekkin3@gmail.com>
2022-08-03 11:09:13 +0300

    feat(mobile): add details (#521)
    * feat: details for mobile
    
    * fix: screenshots approved
    
    * fix: long text removed from mobile details
    
    Co-authored-by: e.nikishina <e.nikishina@MB369886.local>

---
+++ packages/mobile/src/details/Details.tsx
@@ -0,0 +1,158 @@
+import { Box, Pos, Value } from '@qiwi/pijma-core'
+import React, { FC, ReactElement } from 'react'
+
+import { Text } from '../typography'
+
+type DetailsText = ReactElement | string
+
+export interface DetailsProps {
+  children: {
+    title: DetailsText
+    content: DetailsText | DetailsText[]
+  }[]
+  dots?: boolean
+  titleWidth?: Value
+  contentWidth?: Value
+  size?: 's' | 'm'
+  stub?: boolean | number[]
+}
+
+const indentDots: { [size in NonNullable<DetailsProps['size']>]: string } = {
+  s: '6px',
+  m: '8px',
+}
+
+const bottomDots: { [size in NonNullable<DetailsProps['size']>]: string } = {
+  s: '5px',
+  m: '7px',
+}
+
+export const Details: FC<DetailsProps> = ({
+  children,
+  dots,
+  titleWidth,
+  contentWidth,
+  size = 's',
+  stub = false,
+}) =>
+  stub ? (
+    <Box as="dl" width="100%" display="table">
+      {(Array.isArray(stub) ? stub : [0.4, 0.5, 0.3]).map((width, i) => (
+        <Box css={{ display: 'table-row' }} key={`${i}`}>
+          <Box
+            as="dt"
+            width={titleWidth || 0.5}
+            pt={i !== 0 ? 2 : undefined}
+            css={{
+              display: 'table-cell',
+              verticalAlign: 'bottom',
+            }}
+          >
+            {dots ? (
+              <Pos
+                type="relative"
+                overflow="hidden"
+                css={{
+                  ':after': {
+                    content: '""',
+                    position: 'absolute',
+                    bottom: bottomDots[size],
+                    borderBottom: 'dashed 1px #e6e6e6',
+                    width: '100%',
+                    marginLeft: width * 100 + '%',
+                  },
+                }}
+              >
+                <Box width={width} pr={indentDots[size]}>
+                  <Text display="block" size={size} stub />
+                </Box>
+              </Pos>
+            ) : (
+              <Box width={width}>
+                <Text display="block" size={size} stub />
+              </Box>
+            )}
+          </Box>
+          <Box
+            as="dd"
+            width={contentWidth || 0.5}
+            pt={i !== 0 ? 2 : undefined}
+            css={{
+              display: 'table-cell',
+              verticalAlign: 'bottom',
+            }}
+            pl={indentDots[size]}
+          >
+            <Box width={1 - width}>
+              <Text display="block" size={size} stub />
+            </Box>
+          </Box>
+        </Box>
+      ))}
+    </Box>
+  ) : (
+    <Box as="dl" width="100%" display="table">
+      {children.map((item, i) =>
+        ([] as DetailsText[]).concat(item.content).map((content, j) => (
+          <Box css={{ display: 'table-row' }} key={`${i}.${j}`}>
+            <Box
+              as="dt"
+              width={titleWidth}
+              pt={j !== 0 || i !== 0 ? 2 : undefined}
+              css={{
+                display: 'table-cell',
+                verticalAlign: 'bottom',
+              }}
+            >
+              {j === 0 ? (
+                dots ? (
+                  <Pos
+                    type="relative"
+                    overflow="hidden"
+                    css={{
+                      ':after': {
+                        content: '""',
+                        position: 'absolute',
+                        bottom: bottomDots[size],
+                        borderBottom: 'dashed 1px #e6e6e6',
+                        width: '100%',
+                        marginLeft: indentDots[size],
+                      },
+                    }}
+                  >
+                    <Text
+                      bold={false}
+                      size={size}
+                      color="support"
+                      children={item.title}
+                    />
+                  </Pos>
+                ) : (
+                  <Text
+                    color="support"
+                    bold={false}
+                    size={size}
+                    children={item.title}
+                  />
+                )
+              ) : null}
+            </Box>
+            <Box
+              as="dd"
+              width={contentWidth}
+              pt={j !== 0 || i !== 0 ? 2 : undefined}
+              css={{
+                display: 'table-cell',
+                verticalAlign: 'bottom',
+              }}
+              pl={indentDots[size]}
+            >
+              <Text bold={false} size={size} children={content} />
+            </Box>
+          </Box>
+        )),
+      )}
+    </Box>
+  )
+
+Details.displayName = 'Details'
