87f099b8c84d3fe56fb955fe96f6d115b25da930
m.pismenskiy <pismenskiy.m@gmail.com>
2022-08-17 19:06:17 +0300

    feat: add coverage to .gitignore, remove styleguide

--- packages/b2b-desktop/styleguide/components/Link.tsx
+++
@@ -1,6 +0,0 @@
-import { Link, LinkProps } from '@qiwi/pijma-desktop'
-import React, { FC } from 'react'
-
-const LinkRenderer: FC<LinkProps> = (props) => <Link {...props} />
-
-export default LinkRenderer
--- packages/b2b-desktop/styleguide/require.js
+++
@@ -1,4 +0,0 @@
-import * as core from '@qiwi/pijma-core'
-import * as b2bDesktop from '@qiwi/pijma-b2b-desktop'
-
-Object.assign(global, core, b2bDesktop)
