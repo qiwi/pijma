44caed830df259bfcf39c0a4611469945d39d5c8
Dima Livshits <dimonka83@gmail.com>
2022-09-02 09:20:23 +0300

    fix: up deps

--- packages/core/src/main/ts/transition/SimpleTransitionProps.ts
+++ packages/core/src/main/ts/transition/SimpleTransitionProps.ts
@@ -1,6 +1,8 @@
+import { ReactElement } from 'react'
 import { TransitionProps } from 'react-transition-group/Transition'
 
 export type SimpleTransitionProps = TransitionProps & {
+  children?: ReactElement<{ className?: string }>
   enterClassName?: (timeout: number) => string
   enteredClassName?: (timeout: number) => string
   enteringClassName?: (timeout: number) => string
